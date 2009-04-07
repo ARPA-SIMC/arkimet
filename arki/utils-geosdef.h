8f900b8f519a1ef9371caf8e756080fedd06ecb3
Enrico Zini <enrico@enricozini.org>
2009-04-07 17:16:06 +0000

    Added helper headers for GEOS
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@1139 9b5092fc-ae44-42f6-b796-0d08dad2e17b

---
+++ arki/utils-geosdef.h
@@ -0,0 +1,55 @@
+#ifndef ARKI_UTILS_GEOSDEF_H
+#define ARKI_UTILS_GEOSDEF_H
+
+/*
+ * utils-geosdef - libgeos definitions
+ *
+ * Copyright (C) 2009  ARPA-SIM <urpsim@smr.arpa.emr.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Author: Enrico Zini <enrico@enricozini.com>
+ */
+
+#include <arki/utils-geosfwd.h>
+
+#ifdef HAVE_GEOS
+#if GEOS_VERSION < 3
+
+#include <geos/geom.h>
+#ifdef INLINE
+#undef INLINE
+#endif
+
+typedef DefaultCoordinateSequence CoordinateArraySequence;
+
+#else
+
+#include <geos/geom/Coordinate.h>
+#include <geos/geom/CoordinateArraySequence.h>
+#include <geos/geom/GeometryFactory.h>
+#include <geos/geom/GeometryCollection.h>
+#include <geos/geom/Point.h>
+#include <geos/geom/LinearRing.h>
+#include <geos/geom/Polygon.h>
+#ifdef INLINE
+#undef INLINE
+#endif
+
+#endif
+#endif
+
+// vim:set ts=4 sw=4:
+#endif
---
+++ arki/utils-geosfwd.h
@@ -0,0 +1,53 @@
+#ifndef ARKI_UTILS_GEOSDEF_H
+#define ARKI_UTILS_GEOSDEF_H
+
+/*
+ * utils-geosdef - libgeos forward definitions
+ *
+ * Copyright (C) 2009  ARPA-SIM <urpsim@smr.arpa.emr.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Author: Enrico Zini <enrico@enricozini.com>
+ */
+
+#include <arki/config.h>
+
+
+#ifdef HAVE_GEOS
+
+#if GEOS_VERSION < 3
+namespace geos {
+struct Geometry;
+struct GeometryFactory;
+}
+#define ARKI_GEOS_NS geos
+#else
+namespace geos {
+namespace geom {
+struct Geometry;
+struct GeometryFactory;
+}
+}
+#define ARKI_GEOS_NS geos::geom
+#endif
+
+#define ARKI_GEOS_GEOMETRY ARKI_GEOS_NS::Geometry
+#define ARKI_GEOS_GEOMETRYFACTORY ARKI_GEOS_NS::GeometryFactory
+
+#endif
+
+// vim:set ts=4 sw=4:
+#endif
