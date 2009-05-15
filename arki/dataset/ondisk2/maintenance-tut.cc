145d7b8bf4a852159f1d6e8fbd845faec6a673a5
Enrico Zini <enrico@enricozini.org>
2009-05-15 14:13:10 +0000

    Test ondisk2 repack with one file with all data deleted
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@1203 9b5092fc-ae44-42f6-b796-0d08dad2e17b

--- arki/dataset/ondisk2/maintenance-tut.cc
+++ arki/dataset/ondisk2/maintenance-tut.cc
@@ -146,8 +146,6 @@ void to::test<1>()
 	ensure_equals(c.count_ok, 3u);
 	ensure(c.isClean());
 
-	system("find . ");
-
 #if 0
 	// Perform full maintenance and check that things are still ok afterwards
 	stringstream maintlog;
@@ -205,6 +203,57 @@ void to::test<2>()
 	ensure(c.isClean());
 #endif
 }
+
+// Test accuracy of maintenance scan, on dataset with one file to reclaim
+template<> template<>
+void to::test<3>()
+{
+	acquireSamples();
+	{
+		// Remove one element
+		arki::dataset::ondisk2::Writer writer(cfg);
+		writer.remove("2");
+		writer.flush();
+	}
+
+	arki::dataset::ondisk2::Writer writer(cfg);
+	MaintenanceCollector c;
+	writer.maintenance(c);
+
+	ensure_equals(c.fileStates.size(), 3u);
+	ensure_equals(c.count_ok, 2u);
+	ensure_equals(c.count_pack, 0u);
+	ensure_equals(c.count_index, 1u);
+	ensure_equals(c.count_rescan, 0u);
+	ensure_equals(c.count_deleted, 0u);
+	ensure(not c.isClean());
+
+	// Perform packing and check that things are still ok afterwards
+	stringstream s;
+	writer.repack(s, true);
+	ensure_equals(s.str(),
+		"testdir: deleted 2007/07-07.grib1 (34960 freed)\n"
+		"testdir: 1 files deleted, 34960 total bytes freed.\n");
+	c.clear();
+
+	writer.maintenance(c);
+	ensure_equals(c.count_ok, 2u);
+	ensure(c.isClean());
+
+#if 0
+	// Perform full maintenance and check that things are still ok afterwards
+	stringstream maintlog;
+	MetadataCounter counter;
+	FullMaintenance m(maintlog, counter);
+	writer.maintenance(m);
+	ensure_equals(counter.count, 0u);
+	c.clear();
+	writer.maintenance(c);
+	ensure_equals(counter.count, 0u);
+	ensure(c.isClean());
+#endif
+}
+
 
 #if 0
 // Test accuracy of maintenance scan, without index, on dataset with some
