2a35b903d2ee3e6625dc98e45fa6bc65f577923e
Enrico Zini <enrico@enricozini.org>
2009-07-15 14:11:14 +0000

    Added infrastructure to print verbose and debug messages
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@1294 9b5092fc-ae44-42f6-b796-0d08dad2e17b

---
+++ arki/nag.cc
@@ -0,0 +1,94 @@
+/*
+ * nag - Verbose and debug output support
+ *
+ * Copyright (C) 2005--2009  ARPA-SIM <urpsim@smr.arpa.emr.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Author: Enrico Zini <enrico@enricozini.com>
+ */
+
+#include <arki/nag.h>
+
+#include "config.h"
+
+#include <cstdlib>
+#include <cstdio>
+#include <cstdarg>
+
+#ifdef __DATE__
+#define DATE __DATE__
+#else
+#define DATE "??? ?? ????"
+#endif
+
+#ifdef __TIME__
+#define TIME __TIME__
+#else
+#define TIME "??:??:??"
+#endif
+
+namespace arki {
+namespace nag {
+
+static bool _verbose = false;
+static bool _debug = false;
+
+void init(bool verbose, bool debug)
+{
+	_verbose = verbose;
+	if (debug)
+		_debug = _verbose = true;
+
+	if (_verbose)
+	{
+		fprintf(stderr, "%s\n", PACKAGE_NAME " " PACKAGE_VERSION ", compiled on " DATE " " TIME);
+		fprintf(stderr, "%s",
+			"Copyright (C) 2007-2009 ARPA Emilia Romagna.\n"
+			PACKAGE_NAME " comes with ABSOLUTELY NO WARRANTY.\n"
+			"This is free software, and you are welcome to redistribute it and/or modify it\n"
+			"under the terms of the GNU General Public License as published by the Free\n"
+			"Software Foundation; either version 2 of the License, or (at your option) any\n"
+			"later version.\n");
+	}
+}
+
+bool is_verbose() { return _verbose; }
+bool is_debug() { return _debug; }
+
+void verbose(const char* fmt, ...)
+{
+	if (!_verbose) return;
+
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	putc('\n', stderr);
+}
+
+void debug(const char* fmt, ...)
+{
+	if (!_debug) return;
+
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	putc('\n', stderr);
+}
+
+}
+}
---
+++ arki/nag.h
@@ -0,0 +1,56 @@
+/*
+ * nag - Verbose and debug output support
+ *
+ * Copyright (C) 2005--2009  ARPA-SIM <urpsim@smr.arpa.emr.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Author: Enrico Zini <enrico@enricozini.com>
+ */
+
+#ifndef ARKI_NAG_H
+#define ARKI_NAG_H
+
+/** @file
+ * Debugging aid framework that allows to print, at user request, runtime
+ * verbose messages about internal status and operation.
+ */
+
+namespace arki {
+namespace nag {
+
+/**
+ * Initialize the verbose printing interface, taking the allowed verbose level
+ * from the environment and printing a little informational banner if any
+ * level of verbose messages are enabled.
+ */
+void init(bool verbose=false, bool debug=false);
+
+/// Check if verbose output is enabled
+bool is_verbose();
+
+/// Check if debug output is enabled
+bool is_debug();
+
+/// Output a message, if verbose messages are allowed (a newline is automatically appended)
+void verbose(const char* fmt, ...);
+
+/// Output a message, if debug messages are allowed (a newline is automatically appended)
+void debug(const char* fmt, ...);
+
+}
+}
+
+#endif
