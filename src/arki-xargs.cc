d36828a4d3cf40c617ac5fae4bfa7066a903a2d3
Enrico Zini <enrico@enricozini.org>
2009-09-04 18:05:35 +0000

    Cluster data together
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@1400 9b5092fc-ae44-42f6-b796-0d08dad2e17b

--- src/arki-xargs.cc
+++ src/arki-xargs.cc
@@ -28,6 +28,7 @@
 #include <wibble/commandline/parser.h>
 #include <wibble/string.h>
 #include <wibble/sys/fs.h>
+#include <wibble/sys/exec.h>
 
 #include <cstdlib>
 #include <unistd.h>
@@ -63,56 +64,132 @@ struct Options : public StandardParserWithManpage
 }
 }
 
-static std::string makeTempFile(const wibble::sys::Buffer& buf)
+class Clusterer : public MetadataConsumer
 {
-	char fname[] = "arkidata.XXXXXX";
-	int out = mkstemp(fname);
-	if (out < 0)
-		throw wibble::exception::System("creating temporary file");
-	ssize_t res = write(out, buf.data(), buf.size());
-	if (res < 0)
-		throw wibble::exception::File(fname, "writing " + str::fmt(buf.size()));
-	if ((size_t)res != buf.size())
-		throw wibble::exception::Consistency("writing to " + str::fmt(fname), "wrote only " + str::fmt(res) + " bytes out of " + str::fmt(buf.size()));
-	if (close(out) < 0)
-		throw wibble::exception::File(fname, "closing file");
-	return fname;
-}
+protected:
+	const vector<string>& args;
+	string format;
+	size_t count;
+	string tmpfile_name;
+	int tmpfile_fd;
+
+	void startCluster(const std::string& new_format)
+	{
+		char fname[] = "arkidata.XXXXXX";
+		tmpfile_fd = mkstemp(fname);
+		if (tmpfile_fd < 0)
+			throw wibble::exception::System("creating temporary file");
+		tmpfile_name = fname;
+		format = new_format;
+		count = 0;
+	}
+
+	void flushCluster()
+	{
+		if (tmpfile_fd < 0) return;
+
+		// Close the file so that it can be fully read
+		if (close(tmpfile_fd) < 0)
+		{
+			tmpfile_fd = -1;
+			throw wibble::exception::File(tmpfile_name, "closing file");
+		}
+		tmpfile_fd = -1;
+
+		try {
+			// Run process with fname as parameter
+			run_child();
+		} catch (...) {
+			// Use unlink and ignore errors, so that we can
+			// rethrow the right exception here
+			unlink(tmpfile_name.c_str());
+			throw;
+		} 
+
+		// Delete the file, and tolerate if the child process
+		// already deleted it
+		sys::fs::deleteIfExists(tmpfile_name);
+		format.clear();
+		count = 0;
+	}
+
+	void run_child()
+	{
+		if (count == 0) return;
+
+		sys::Exec child(args[0]);
+		child.args = args;
+		child.args.push_back(tmpfile_name);
+		child.envFromParent = false;
+		child.searchInPath = true;
+		child.importEnv();
+
+		child.env.push_back("ARKI_XARGS_FORMAT=" + str::toupper(format));
+		child.env.push_back("ARKI_XARGS_COUNT=" + str::fmt(count));
+
+		child.fork();
+		int res = child.wait();
+		if (res != 0)
+			throw wibble::exception::Consistency("running " + args[0], "process returned exit status " + str::fmt(res));
+	}
+
+	virtual void add_to_cluster(Metadata& md)
+	{
+		sys::Buffer buf = md.getData();
+		ssize_t res = write(tmpfile_fd, buf.data(), buf.size());
+		if (res < 0)
+			throw wibble::exception::File(tmpfile_name, "writing " + str::fmt(buf.size()));
+		if ((size_t)res != buf.size())
+			throw wibble::exception::Consistency("writing to " + tmpfile_name, "wrote only " + str::fmt(res) + " bytes out of " + str::fmt(buf.size()));
+		++count;
+	}
+
+public:
+	Clusterer(const vector<string>& args) : args(args), tmpfile_fd(-1) {}
+	~Clusterer()
+	{
+		if (tmpfile_fd >= 0)
+			flushCluster();
+	}
+
+	virtual bool operator()(Metadata& md)
+	{
+		if (format.empty() || format != md.source->format)
+		{
+			flush();
+			startCluster(md.source->format);
+		}
+
+		add_to_cluster(md);
+
+		return true;
+	}
+
+	void flush()
+	{
+		flushCluster();
+	}
+};
 
 static void process(const vector<string>& args, runtime::Input& in)
 {
-	Metadata md;
 	wibble::sys::Buffer buf;
 	string signature;
 	unsigned version;
+
+	Clusterer consumer(args);
+
 	while (types::readBundle(in.stream(), in.name(), buf, signature, version))
 	{
-		if (signature == "MD" || signature == "!D")
-		{
-			md.read(buf, version, in.name());
-			if (md.source->style() == types::Source::INLINE)
-				md.readInlineData(in.stream(), in.name());
-			// Save data to temporary file
-			string fname = makeTempFile(md.getData());
+		if (signature != "MD") continue;
 
-			try {
-				// TODO: Run process with fname as parameter
-				;
-			} catch (...) {
-				// Use unlink and ignore errors, so that we can
-				// rethrow the right exception here
-				unlink(fname.c_str());
-				throw;
-			} 
-
-			// Delete the file, and tolerate if the child process
-			// already deleted it
-			sys::fs::deleteIfExists(fname);
-		}
-		else if (signature == "SU")
-		{
-			continue;
-		}
+		// Read the metadata
+		Metadata md;
+		md.read(buf, version, in.name());
+		if (md.source->style() == types::Source::INLINE)
+			md.readInlineData(in.stream(), in.name());
+		if (!consumer(md))
+			break;
 	}
 }
 
