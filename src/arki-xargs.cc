a82861aa42e5095339da4cb3d80b0d81c1109a7f
Enrico Zini <enrico@enricozini.org>
2009-09-04 17:31:17 +0000

    arki-xargs: added --marx-args, --max-size, --time-interval
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@1404 9b5092fc-ae44-42f6-b796-0d08dad2e17b

--- src/arki-xargs.cc
+++ src/arki-xargs.cc
@@ -21,6 +21,7 @@
  */
 
 #include <arki/metadata.h>
+#include <arki/types/reftime.h>
 #include <arki/scan/any.h>
 #include <arki/runtime.h>
 
@@ -31,6 +32,7 @@
 #include <wibble/sys/exec.h>
 
 #include <cstdlib>
+#include <cstring>
 #include <unistd.h>
 
 #include "config.h"
@@ -44,7 +46,10 @@ namespace commandline {
 
 struct Options : public StandardParserWithManpage
 {
-	wibble::commandline::VectorOption<wibble::commandline::String>* inputfiles;
+	VectorOption<String>* inputfiles;
+	IntOption* max_args;
+	StringOption* max_bytes;
+	StringOption* time_interval;
 
 	Options() : StandardParserWithManpage("arki-xargs", PACKAGE_VERSION, 1, PACKAGE_BUGREPORT)
 	{
@@ -55,6 +60,20 @@ struct Options : public StandardParserWithManpage
 			"'command [initial-arguments] filename' on it";
 		inputfiles = add< VectorOption<String> >("input", 'i', "input", "file",
 			"read data from this file instead of standard input (can be given more than once)");
+
+		max_args = add<IntOption>("max-args", 'n', "max-args", "count",
+			"group at most this amount of data items per command invocation");
+		max_bytes = add<StringOption>("max-size", 's', "max-size", "size",
+			"create data files no bigger than this size. This may "
+			"NOT be respected if there is one single data item "
+			"greater than the size specified.  size may be followed "
+			"by the following multiplicative suffixes: c=1, w=2, "
+			"b=512, kB=1000, K=1024, MB=1000*1000, M=1024*1024, "
+			"xM=M GB=1000*1000*1000, G=1024*1024*1024, and so on "
+			"for T, P, E, Z, Y.");
+		time_interval = add<StringOption>("time-interval", 0, "time-interval", "interval",
+			"create one data file per 'interval', where interval "
+			"can be minute, hour, day, month or year");
 
 		// We need to pass switches in [initial-arguments] untouched
 		no_switches_after_first_arg = true;
@@ -70,8 +89,10 @@ protected:
 	const vector<string>& args;
 	string format;
 	size_t count;
+	size_t size;
 	string tmpfile_name;
 	int tmpfile_fd;
+	int cur_interval[6];
 
 	void startCluster(const std::string& new_format)
 	{
@@ -82,6 +103,7 @@ protected:
 		tmpfile_name = fname;
 		format = new_format;
 		count = 0;
+		size = 0;
 	}
 
 	void flushCluster()
@@ -111,6 +133,8 @@ protected:
 		sys::fs::deleteIfExists(tmpfile_name);
 		format.clear();
 		count = 0;
+		size = 0;
+		cur_interval[0] = -1;
 	}
 
 	void run_child()
@@ -132,19 +156,66 @@ protected:
 			throw wibble::exception::Consistency("running " + args[0], "process returned exit status " + str::fmt(res));
 	}
 
-	virtual void add_to_cluster(Metadata& md)
+	void md_to_interval(Metadata& md, int* interval) const
 	{
-		sys::Buffer buf = md.getData();
+		UItem<types::Reftime> rt = md.get(types::TYPE_REFTIME).upcast<types::Reftime>();
+		if (!rt.defined())
+			throw wibble::exception::Consistency("computing time interval", "metadata has no reference time");
+		UItem<types::Time> t;
+		switch (rt->style())
+		{
+			case types::Reftime::POSITION: t = rt.upcast<types::reftime::Position>()->time; break;
+			case types::Reftime::PERIOD: t = rt.upcast<types::reftime::Period>()->end; break;
+			default:
+				throw wibble::exception::Consistency("computing time interval", "reference time has invalid style: " + types::Reftime::formatStyle(rt->style()));
+		}
+		for (unsigned i = 0; i < 6; ++i)
+			interval[i] = i < max_interval ? t->vals[i] : -1;
+	}
+
+	void add_to_cluster(Metadata& md, sys::Buffer& buf)
+	{
 		ssize_t res = write(tmpfile_fd, buf.data(), buf.size());
 		if (res < 0)
 			throw wibble::exception::File(tmpfile_name, "writing " + str::fmt(buf.size()));
 		if ((size_t)res != buf.size())
 			throw wibble::exception::Consistency("writing to " + tmpfile_name, "wrote only " + str::fmt(res) + " bytes out of " + str::fmt(buf.size()));
 		++count;
+		size += buf.size();
+		if (cur_interval[0] == -1 && max_interval != 0)
+			md_to_interval(md, cur_interval);
+	}
+
+	bool exceeds_count(Metadata& md) const
+	{
+		return (max_args != 0 && count >= max_args);
+	}
+
+	bool exceeds_size(sys::Buffer& buf) const
+	{
+		if (max_bytes == 0 || size == 0) return false;
+		return size + buf.size() > max_bytes;
+	}
+
+	bool exceeds_interval(Metadata& md) const
+	{
+		if (max_interval == 0) return false;
+		if (cur_interval[0] == -1) return false;
+		int candidate[6];
+		md_to_interval(md, candidate);
+		return memcmp(cur_interval, candidate, 6*sizeof(int)) != 0;
 	}
 
 public:
-	Clusterer(const vector<string>& args) : args(args), tmpfile_fd(-1) {}
+	size_t max_args;
+	size_t max_bytes;
+	size_t max_interval;
+
+	Clusterer(const vector<string>& args) :
+		args(args), tmpfile_fd(-1), max_args(0), max_bytes(0), max_interval(0)
+	{
+		cur_interval[0] = -1;
+	}
 	~Clusterer()
 	{
 		if (tmpfile_fd >= 0)
@@ -153,13 +224,16 @@ public:
 
 	virtual bool operator()(Metadata& md)
 	{
-		if (format.empty() || format != md.source->format)
+		sys::Buffer buf = md.getData();
+
+		if (format.empty() || format != md.source->format ||
+		    exceeds_count(md) || exceeds_size(buf) || exceeds_interval(md))
 		{
 			flush();
 			startCluster(md.source->format);
 		}
 
-		add_to_cluster(md);
+		add_to_cluster(md, buf);
 
 		return true;
 	}
@@ -190,6 +264,46 @@ static void process(Clusterer& consumer, runtime::Input& in)
 	}
 }
 
+static size_t parse_size(const std::string& str)
+{
+	const char* s = str.c_str();
+	char* e;
+	unsigned long long int res = strtoull(s, &e, 10);
+	string suffix = str.substr(e-s);
+	if (suffix.empty() || suffix == "c")
+		return res;
+	if (suffix == "w") return res * 2;
+	if (suffix == "b") return res * 512;
+	if (suffix == "kB") return res * 1000;
+	if (suffix == "K") return res * 1024;
+	if (suffix == "MB") return res * 1000*1000;
+	if (suffix == "M" || suffix == "xM") return res * 1024*1024;
+	if (suffix == "GB") return res * 1000*1000*1000;
+	if (suffix == "G") return res * 1024*1024*1024;
+	if (suffix == "TB") return res * 1000*1000*1000*1000;
+	if (suffix == "T") return res * 1024*1024*1024*1024;
+	if (suffix == "PB") return res * 1000*1000*1000*1000*1000;
+	if (suffix == "P") return res * 1024*1024*1024*1024*1024;
+	if (suffix == "EB") return res * 1000*1000*1000*1000*1000*1000;
+	if (suffix == "E") return res * 1024*1024*1024*1024*1024*1024;
+	if (suffix == "ZB") return res * 1000*1000*1000*1000*1000*1000*1000;
+	if (suffix == "Z") return res * 1024*1024*1024*1024*1024*1024*1024;
+	if (suffix == "YB") return res * 1000*1000*1000*1000*1000*1000*1000*1000;
+	if (suffix == "Y") return res * 1024*1024*1024*1024*1024*1024*1024*1024;
+	throw wibble::exception::Consistency("parsing size", "unknown suffix: '"+suffix+"'");
+}
+
+static size_t parse_interval(const std::string& str)
+{
+        string name = str::trim(str::tolower(str));
+        if (name == "minute") return 5;
+        if (name == "hour") return 4;
+        if (name == "day") return 3;
+        if (name == "month") return 2;
+        if (name == "year") return 1;
+        throw wibble::exception::Consistency("parsing interval name", "unsupported interval: " + str + ".  Valid intervals are minute, hour, day, month and year");
+}
+
 int main(int argc, const char* argv[])
 {
 	wibble::commandline::Options opts;
@@ -207,6 +321,12 @@ int main(int argc, const char* argv[])
 		runtime::init();
 
 		Clusterer consumer(args);
+		if (opts.max_args->isSet())
+			consumer.max_args = opts.max_args->intValue();
+		if (opts.max_bytes->isSet())
+			consumer.max_bytes = parse_size(opts.max_bytes->stringValue());
+		if (opts.time_interval->isSet())
+			consumer.max_interval = parse_interval(opts.time_interval->stringValue());
 
 		if (opts.inputfiles->values().empty())
 		{
