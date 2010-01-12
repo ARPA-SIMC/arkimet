38156b24ca990b151c9a3600218ff3dd3eaf4db0
Enrico Zini <enrico@enricozini.org>
2010-01-12 15:28:43 +0000

    Generate the retrieval space
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@1495 9b5092fc-ae44-42f6-b796-0d08dad2e17b

--- src/arki-retrieve.cc
+++ src/arki-retrieve.cc
@@ -29,6 +29,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 
 #include "config.h"
 
@@ -55,6 +56,110 @@ struct Options : public StandardParserWithManpage
 }
 }
 
+/**
+ * Define a linear metadata space.
+ *
+ * For every metadata type in the soup, we have a number of elements we want.
+ *
+ * Every metadata code in the soup defines a dimension of a matrix. The number
+ * of items requested for that metadata code defines the number of elements of
+ * the matrix across that dimension.
+ *
+ * Since the metadata codes are sorted, and the items are sorted, and the soup
+ * (that is, the topology of the matrix) will not change, we can define an
+ * unchanging bijective function between the matrix elements and a segment in
+ * N. This means that we can associate to each matrix element an integer number
+ * between 0 and the number of items in the matrix.
+ *
+ * What is required at the end of the game is to have one and only one metadata
+ * item per matrix element.
+ */
+struct MDGrid
+{
+	std::map<types::Code, std::vector< Item<> > > soup;
+	std::vector<size_t> dim_sizes;
+	size_t maxidx;
+
+	// Find the linearised matrix index for md. Returns -1 if md does not
+	// match a point in the matrix
+	int index(const Metadata& md)
+	{
+		int res = 0;
+		// Consolidate the soup space: remove duplicates, sort the vectors
+		size_t dim = 0;
+                for (std::map<types::Code, std::vector< Item<> > >::iterator i = soup.begin();
+				i != soup.end(); ++i, ++dim)
+		{
+			UItem<> mdi = md.get(i->first);
+			if (!mdi.defined())
+				return -1;
+			vector< Item<> >::const_iterator lb =
+				lower_bound(i->second.begin(), i->second.end(), mdi);
+			if (*lb != mdi)
+				return -1;
+			int idx = lb - i->second.begin();
+			res += idx * dim_sizes[dim];
+		}
+		return res;
+	}
+
+	std::vector< Item<> > expand(size_t index)
+	{
+		vector< Item<> > res;
+		size_t dim = 0;
+                for (std::map<types::Code, std::vector< Item<> > >::iterator i = soup.begin();
+				i != soup.end(); ++i, ++dim)
+		{
+			size_t idx = index / dim_sizes[dim];
+			res.push_back(i->second[idx]);
+			index = index % dim_sizes[dim];
+		}
+		return res;
+	}
+
+	void read(std::istream& input, const std::string& fname)
+	{
+		string line;
+		while (!input.eof())
+		{
+			getline(input, line);
+			if (input.fail() && !input && !input.eof())
+				throw wibble::exception::File(fname, "reading one line");
+			line = str::trim(line);
+			if (line.empty())
+				continue;
+			size_t pos = line.find(':');
+			if (pos == string::npos)
+			{
+				throw wibble::exception::Consistency("parsing file " + fname,
+						"cannot parse line \"" + line + "\"");
+			}
+			types::Code code = types::parseCodeName(line.substr(0, pos));
+			Item<> item = decodeString(code, str::trim(line.substr(pos+1)));
+			soup[code].push_back(item);
+		}
+
+		// Consolidate the soup space: remove duplicates, sort the vectors
+		maxidx = soup.empty() ? 0 : 1;
+                for (std::map<types::Code, std::vector< Item<> > >::iterator i = soup.begin();
+				i != soup.end(); ++i)
+		{
+			// Copy to a set then back to the vector
+			set< Item<> > s;
+			std::copy(i->second.begin(), i->second.end(), inserter(s, s.begin()));
+			i->second.clear();
+			std::copy(s.begin(), s.end(), back_inserter(i->second));
+
+			// Update the number of matrix elements below every dimension
+			for (vector<size_t>::iterator j = dim_sizes.begin();
+					j != dim_sizes.end(); ++j)
+				*j *= i->second.size();
+			dim_sizes.push_back(1);
+			maxidx *= i->second.size();
+		}
+	}
+};
+
 int main(int argc, const char* argv[])
 {
 	wibble::commandline::Options opts;
@@ -67,39 +172,33 @@ int main(int argc, const char* argv[])
 		if (!opts.hasNext())
 			throw wibble::exception::BadOption("you need to provide a dataset or data file name");
 
+		MDGrid mdgrid;
+
 		// Read the metadata soup
 		string file = "-";
 		runtime::Input in(file);
-		std::istream& input = in.stream();
+		mdgrid.read(in.stream(), in.name());
 
-		std::map<types::Code, std::set< Item<> > > soup;
-
-		string line;
-		while (!input.eof())
+		cerr << "Metadata soup:" << endl;
+                for (std::map<types::Code, std::vector< Item<> > >::const_iterator i = mdgrid.soup.begin();
+				i != mdgrid.soup.end(); ++i)
 		{
-			getline(input, line);
-			if (input.fail() && !input && !input.eof())
-				throw wibble::exception::File(file, "reading one line");
-			line = str::trim(line);
-			if (line.empty())
-				continue;
-			size_t pos = line.find(':');
-			if (pos == string::npos)
+			cerr << " " << types::tag(i->first) << ":" << endl;
+			for (std::vector< Item<> >::const_iterator j = i->second.begin();
+					j != i->second.end(); ++j)
 			{
-				throw wibble::exception::Consistency("parsing file " + in.name(),
-						"cannot parse line \"" + line + "\"");
+				cerr << "  " << *j << endl;
 			}
-			types::Code code = types::parseCodeName(line.substr(0, pos));
-			Item<> item = decodeString(code, str::trim(line.substr(pos+1)));
-			soup[code].insert(item);
 		}
 
-                for (std::map<types::Code, std::set< Item<> > >::const_iterator i = soup.begin();
-				i != soup.end(); ++i)
+		cerr << "Number of combinations: " << mdgrid.maxidx << endl;
+
+		for (size_t i = 0; i < mdgrid.maxidx; ++i)
 		{
-			cerr << types::tag(i->first) << ":" << endl;
-			for (std::set< Item<> >::const_iterator j = i->second.begin();
-					j != i->second.end(); ++j)
+			cerr << " Combination " << i << ":" << endl;
+			vector< Item<> > c = mdgrid.expand(i);
+			for (vector< Item<> >::const_iterator j = c.begin();
+					j != c.end(); ++j)
 			{
 				cerr << "  " << *j << endl;
 			}
