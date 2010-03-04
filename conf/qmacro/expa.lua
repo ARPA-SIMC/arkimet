7388cf520757818520c6f5862eab34ebe26988ed
Enrico Zini <enrico@enricozini.org>
2010-03-04 11:58:34 +0000

    Redone expa macro
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@1696 9b5092fc-ae44-42f6-b796-0d08dad2e17b

--- conf/qmacro/expa.lua
+++ conf/qmacro/expa.lua
@@ -1,6 +1,12 @@
 -- Build the merged query
--- linepat = "^ds:(.-)%.%s+d:(.-)%.%s+t:(.-)%.%s+s:(.-)%.%s+l:(.-)%.%s+v:(.-)%.$"
-linepat =    "^ds:(.-)%.%s+d:(.-)%.%s+t:(.-)%.%s+s:(.-)%.%s+l:(.-)%.%s+v:(.-)%.%s*"
+linepat = "^ds:(.-)%.%s+d:(.-)%.%s+t:(.-)%.%s+s:(.-)%.%s+l:(.-)%.%s+v:(.-)%.%s*$"
+
+-- TODO: replace with QueryMacro passing verbose settings in variables
+verbose = os.getenv("VERBOSE")
+
+dataset_aliases = {
+	LMSMR = { "lmsmr4x46", "lmsmr4x45", "lmsmr4x47" }
+}
 
 Set = {}
 
@@ -48,109 +54,147 @@ function buildmatcher(s, l, v)
 end
 
 
-DSInfo = {}
+QueryChunk = {}
 
-function DSInfo:new(o)
+function QueryChunk:new(o)
 	o = o or {}
 	setmetatable(o, self)
 	self.__index = self
 	return o
 end
 
-function DSInfo:init(dsname)
-	local ds = qmacro:dataset(dsname)
-	if ds == nil then error("Cannot access dataset " .. dsname) end
-	self["dataset"] = ds
-	local gq = arki.gridquery.new(ds)
-	self["gq"] = gq
-end
+function QueryChunk:init(dsname, d, t)
+	self["dsname"] = dsname
+	self["date"] = d
+	self["time"] = t
+	self["queries"] = {}
 
-
--- Cache of summary information
-dsinfo = {}
-
--- Parse input
-all_ds = Set:new()
-all_s = Set:new()
-all_l = Set:new()
-all_v = Set:new()
-for line in query:gmatch("[^\r\n]+") do
-	ds, d, t, s, l, v = line:match(linepat)
-	if ds ~= nil then
-		ds = Set:parse(ds)
-		d = Set:parse(d)
-		s = Set:parse(s)
-		l = Set:parse(l)
-		v = Set:parse(v)
-
-		for name, _ in pairs(ds) do
-			info = dsinfo[name]
-			if dsinfo[name] == nil then
-				info = DSInfo:new()
-				info:init(name)
-				dsinfo[name] = info
-			end
-			q = buildmatcher(s, l, v)
-			-- print("ADD", name, q)
-			info.gq:add(q)
-
-			-- Add times
-			for k, _ in pairs(d) do
-				if k == "@" then
-					-- Expand '@' in yesterday
-					yesterday = os.time() - (3600*24)
-					k = os.date("%Y-%m-%d 00:00:00", yesterday)
-				end
-				info.gq:addtime(k)
-			end
-		end
-
-		all_ds:addset(ds)
-		all_s:addset(s)
-		all_l:addset(l)
-		all_v:addset(v)
+	-- Parse date
+	if d:match("^%s*[Tt]%s*$") then
+		d = os.date("%Y-%m-%d")
+	elseif d:match("^%s*@%s*$") then
+		d = os.getenv("DAY")
+		if d == nil then error("If using @, set $DAY to YYYY-MM-DD") end
 	else
-		print("line not parsed:", line)
+		pm, val = d:match("^%s*[Tt]%s*([+-])%s*(%d+)%s*$")
+		if pm == nil then error("Expected 'T+n' or 'T-n'") end
+		if pm == "+" then
+			ts = os.time() + 3600 * 24 * val
+		else
+			ts = os.time() - 3600 * 24 * val
+		end
+		d = os.date("%Y-%m-%d", ts)
 	end
+
+	-- Parse time
+	h, m = t:match("(%d%d)(%d%d)")
+	if h == nil then error("Time expected in form HHMM") end
+
+	self.timestr = d .. string.format(" %02d:%02d:00", h, m)
 end
 
--- Consolidate the query grids
-for name, info in pairs(dsinfo) do
-	info.gq:consolidate()
-	-- print ("MQ", name, info.gq:mergedquery())
+function QueryChunk:addquery(q)
+	table.insert(self.queries, q)
 end
 
-function queryData(q, cons)
-	for name, info in pairs(dsinfo) do
+function QueryChunk:datasets()
+	local res = dataset_aliases[self.dsname]
+	if res ~= nil then return res end
+	return { self.dsname }
+end
+
+-- Feed all information we have into a GridQuery
+function QueryChunk:setupgq(gq)
+	-- Add queries
+	for _, q in ipairs(self.queries) do
+		gq:add(q)
+	end
+
+	-- Add time
+	gq:addtime(self.timestr)
+
+	-- Consolidate in the end
+	gq:consolidate()
+end
+
+function QueryChunk:queryData(cons)
+	for idx, dsname in ipairs(self:datasets()) do
+		if verbose then io.stderr:write("Trying query on ", dsname, "\n") end
+
+		local ds = qmacro:dataset(dsname)
+		if ds == nil then error("Cannot access dataset " .. dsname) end
+		local gq = arki.gridquery.new(ds)
+
+		-- Prepare the GridQuery
+		self:setupgq(gq)
+
 		-- Build the merged query
-		query = info.gq:mergedquery()
+		query = gq:mergedquery()
 		-- print (query)
 		-- print (query:expanded())
-
+	
 		-- Query dataset storing results
 		mds = {}
-		info.dataset:queryData({matcher=query}, function(md)
-			if info.gq:checkandmark(md) then
+		ds:queryData({matcher=query}, function(md)
+			if gq:checkandmark(md) then
 				table.insert(mds, md:copy())
 			end
 			return true
 		end)
 
-		if info.gq:satisfied() then
+		if gq:satisfied() then
+			if verbose then io.stderr:write("Satisfied query on ", dsname, "\n") end
 			for idx, md in ipairs(mds) do
 				cons(md)
 			end
-		else
-			print("Master query: " .. tostring(query))
-			print(info.gq:dump())
-			error("Query cannot be satisfied on dataset " .. name)
+			return
+		elseif verbose then
+			io.stderr:write("Unsatisfied query on ", dsname, "\n")
+			io.stderr:write("Master query: ", tostring(query), "\n")
+			io.stderr:write("Result space:\n")
+			io.stderr:write(gq:dump(), "\n")
 		end
+	end
+
+	error("Query cannot be satisfied")
+end
+
+
+-- Cache of summary information
+chunks = {}
+
+-- Parse input
+for line in query:gmatch("[^\r\n]+") do
+	ds, d, t, s, l, v = line:match(linepat)
+	if ds ~= nil then
+		s = Set:parse(s)
+		l = Set:parse(l)
+		v = Set:parse(v)
+		q = buildmatcher(s, l, v)
+
+		dstag = ds .. "/" .. d .. "/" .. t
+		info = chunks[dstag]
+		if chunks[dstag] == nil then
+			info = QueryChunk:new()
+			info:init(ds, d, t)
+			chunks[dstag] = info
+		end
+		-- print("ADD", name, q)
+		info:addquery(q)
+	else
+		error("line not parsed:", line)
+	end
+end
+
+function queryData(q, cons)
+	for _, info in pairs(chunks) do
+		info:queryData(cons)
 	end
 end
 
 function querySummary(q, sum)
 	s = arki.summary.new()
-	for name, info in pairs(dsinfo) do
+	for name, info in pairs(chunks) do
 		-- Build the merged query
 		query = info.gq:mergedquery()
 		-- print (query)
