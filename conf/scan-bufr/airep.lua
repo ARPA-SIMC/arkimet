d947d350704d3ccf064f1741b22e1a42db25c65d
Enrico Zini <enrico@enricozini.org>
2010-06-09 15:55:36 +0000

    Added scanning scripts for more message types
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@1990 9b5092fc-ae44-42f6-b796-0d08dad2e17b

---
+++ conf/scan-bufr/airep.lua
@@ -0,0 +1,14 @@
+function scan(msg, md)
+	area = {}
+	lat = msg:find('latitude')
+	lon = msg:find('longitude')
+	blo = msg:find('block')
+	sta = msg:find('station')
+	id = msg:find('ident')
+	if lat then area.lat = sta:enqi() end
+	if lon then area.lon = sta:enqi() end
+	if blo then area.blo = blo:enqi() end
+	if sta then area.sta = sta:enqi() end
+	if id then area.id = id:enqc() end
+	md:set(arki_area.grib(area))
+end
