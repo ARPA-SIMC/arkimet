0127792ff1db77ed60fe425be55fff68d26a471f
Enrico Zini <enrico@enricozini.org>
2010-06-09 15:58:59 +0000

    Tried some of the new scanning scripts
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@1992 9b5092fc-ae44-42f6-b796-0d08dad2e17b

--- conf/scan-bufr/temp_ship.lua
+++ conf/scan-bufr/temp_ship.lua
@@ -5,8 +5,8 @@ function scan(msg, md)
 	blo = msg:find('block')
 	sta = msg:find('station')
 	id = msg:find('ident')
-	if lat then area.lat = sta:enqi() end
-	if lon then area.lon = sta:enqi() end
+	if lat then area.lat = lat:enqi() end
+	if lon then area.lon = lon:enqi() end
 	if blo then area.blo = blo:enqi() end
 	if sta then area.sta = sta:enqi() end
 	if id then area.id = id:enqc() end
