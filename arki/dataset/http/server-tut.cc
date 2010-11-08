e5535a4e4d6029e45fd6200b1fcb8a3049c1744c
Enrico Zini <enrico@enricozini.org>
2010-11-08 22:48:43 +0000

    Scaffhold for server-side dataset wrappers
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@2221 9b5092fc-ae44-42f6-b796-0d08dad2e17b

---
+++ arki/dataset/http/server-tut.cc
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2010  Enrico Zini <enrico@enricozini.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ */
+
+#include <arki/tests/test-utils.h>
+#include <arki/dataset/http/server.h>
+
+#include <sstream>
+#include <iostream>
+
+namespace tut {
+using namespace std;
+using namespace arki;
+
+struct arki_dataset_http_server_shar {
+    arki_dataset_http_server_shar()
+    {
+    }
+};
+TESTGRP(arki_dataset_http_server);
+
+template<> template<>
+void to::test<1>()
+{
+}
+
+}
+
+// vim:set ts=4 sw=4:
