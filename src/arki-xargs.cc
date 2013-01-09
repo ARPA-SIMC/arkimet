6f46c66b339d0d5ef8088b7136fb7c71b8394af2
Enrico Zini <enrico@enricozini.org>
2013-01-09 12:27:47 +0000

    arki-xargs serialization
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@2710 9b5092fc-ae44-42f6-b796-0d08dad2e17b

--- src/arki-xargs.cc
+++ src/arki-xargs.cc
@@ -27,6 +27,7 @@
 #include <arki/types/timerange.h>
 #include <arki/scan/any.h>
 #include <arki/runtime.h>
+#include <arki/data.h>
 
 #include <wibble/exception.h>
 #include <wibble/commandline/parser.h>
@@ -192,13 +193,15 @@ protected:
 
 	void add_to_cluster(Metadata& md, sys::Buffer& buf)
 	{
-		ssize_t res = write(tmpfile_fd, buf.data(), buf.size());
+        std::stringstream stream;
+        arki::data::OstreamWriter::get(md.source->format)->stream(md, stream);
+        ssize_t res = write(tmpfile_fd, stream.str().c_str(), stream.str().size());
 		if (res < 0)
 			throw wibble::exception::File(tmpfile_name, "writing " + str::fmt(buf.size()));
-		if ((size_t)res != buf.size())
-			throw wibble::exception::Consistency("writing to " + tmpfile_name, "wrote only " + str::fmt(res) + " bytes out of " + str::fmt(buf.size()));
+		if ((size_t)res != stream.str().size())
+			throw wibble::exception::Consistency("writing to " + tmpfile_name, "wrote only " + str::fmt(res) + " bytes out of " + str::fmt(stream.str().size()));
 		++count;
-		size += buf.size();
+		size += res;
 		if (cur_interval[0] == -1 && max_interval != 0)
 			md_to_interval(md, cur_interval);
 		timespan.merge(md.get(types::TYPE_REFTIME).upcast<types::Reftime>());
