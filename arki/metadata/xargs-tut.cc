387c1b621dffb6b46c1fb38546cc814d61c7198e
Daniele Branchini <dbranchini@arpa.emr.it>
2013-10-23 13:25:05 +0000

    Deal with shells that only quote env args when needed
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@2825 9b5092fc-ae44-42f6-b796-0d08dad2e17b

--- arki/metadata/xargs-tut.cc
+++ arki/metadata/xargs-tut.cc
@@ -70,7 +70,7 @@ void to::test<2>()
     metadata::Xargs xargs;
     xargs.command.push_back("/bin/sh");
     xargs.command.push_back("-c");
-    xargs.command.push_back("set | grep ARKI_XARGS_ |tee /tmp/zz >> tmp-xargs");
+    xargs.command.push_back("set | grep ARKI_XARGS_ >> tmp-xargs");
     xargs.filename_argument = 1000; // Do not pass the file name
 
     xargs.max_count = 10;
@@ -79,9 +79,9 @@ void to::test<2>()
     xargs.flush();
 
     string out = utils::files::read_file("tmp-xargs");
-    wassert(actual(out).contains("ARKI_XARGS_FILENAME='"));
-    wassert(actual(out).contains("ARKI_XARGS_FORMAT='GRIB1'\n"));
-    wassert(actual(out).contains("ARKI_XARGS_COUNT='10'\n"));
+    wassert(actual(out).contains("ARKI_XARGS_FILENAME="));
+    wassert(actual(out).matches("ARKI_XARGS_FORMAT=['\"]?GRIB1['\"]?\n"));
+    wassert(actual(out).matches("ARKI_XARGS_COUNT=['\"]?10['\"]?\n"));
     wassert(actual(out).contains("ARKI_XARGS_TIME_START='2007-07-08 13:00:00Z'\n"));
     wassert(actual(out).contains("ARKI_XARGS_TIME_END='2007-07-08 13:00:00Z'\n"));
 }
