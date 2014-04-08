36279572ca8bbfec07cebe7107ac06dda0c7a835
Enrico Zini <enrico@enricozini.org>
2014-04-08 12:55:29 +0000

    Scaffold for new dir-based data source
    git-svn-id: https://svn.smr.arpa.emr.it/arkimet/arkimet/trunk@2883 9b5092fc-ae44-42f6-b796-0d08dad2e17b

---
+++ arki/data/dir-tut.cc
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2014  ARPA-SIM <urpsim@smr.arpa.emr.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Author: Enrico Zini <enrico@enricozini.com>
+ */
+
+#include "config.h"
+
+#include "arki/tests/tests.h"
+#include "arki/metadata/tests.h"
+#include "arki/metadata/collection.h"
+#include "arki/data.h"
+#include "arki/data/dir.h"
+#include "arki/scan/any.h"
+#include "arki/utils/files.h"
+#include <wibble/sys/fs.h>
+#include <sstream>
+
+namespace std {
+static inline std::ostream& operator<<(std::ostream& o, const arki::Metadata& m)
+{
+        m.writeYaml(o);
+            return o;
+}
+}
+
+namespace tut {
+using namespace std;
+using namespace arki;
+using namespace arki::data;
+using namespace arki::utils;
+using namespace wibble;
+using namespace wibble::tests;
+
+struct arki_data_dir_shar {
+    arki_data_dir_shar()
+    {
+    }
+};
+
+TESTGRP(arki_data_dir);
+
+// Try to append some data
+template<> template<>
+void to::test<1>()
+{
+}
+
+}
+
