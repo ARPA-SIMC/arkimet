4e324c65b2656e98b6f655c27db114f0dd1575da
Enrico Zini <enrico@enricozini.org>
2015-11-25 17:24:33 +0100

    Dewibbled types/utils header

--- arki/types/utils.h
+++ arki/types/utils.h
@@ -1,31 +1,10 @@
 #ifndef ARKI_TYPES_UTILS_H
 #define ARKI_TYPES_UTILS_H
 
-/*
- * types/utils - arkimet metadata type system implementation utilities
- *
- * Copyright (C) 2007--2011  ARPA-SIM <urpsim@smr.arpa.emr.it>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Author: Enrico Zini <enrico@enricozini.com>
- */
-
 #include <arki/types.h>
-#include <wibble/exception.h>
+#include <stdexcept>
 #include <string>
+#include <sstream>
 #include <set>
 #include <cstring>
 
@@ -128,17 +107,34 @@ void debug_intern_stats();
 template<typename T>
 static typename T::Style outerParse(const std::string& str, std::string& inner)
 {
-	using namespace std;
-
-	if (str.empty())
-		throw wibble::exception::Consistency(string("parsing ") + typeid(T).name(), "string is empty");
-	size_t pos = str.find('(');
-	if (pos == string::npos)
-		throw wibble::exception::Consistency(string("parsing ") + typeid(T).name(), "no open parentesis found in \""+str+"\"");
-	if (str[str.size() - 1] != ')')
-		throw wibble::exception::Consistency(string("parsing ") + typeid(T).name(), "string \""+str+"\" does not end with closed parentesis");
-	inner = str.substr(pos+1, str.size() - pos - 2);
-	return T::parseStyle(str.substr(0, pos));
+    if (str.empty())
+    {
+        std::string msg("cannot parse ");
+        msg += typeid(T).name();
+        msg += ": string is empty";
+        throw std::runtime_error(std::move(msg));
+    }
+    size_t pos = str.find('(');
+    if (pos == std::string::npos)
+    {
+        std::string msg("cannot parse ");
+        msg += typeid(T).name();
+        msg += ": no open parenthesis found in '";
+        msg += str;
+        msg += "'";
+        throw std::runtime_error(std::move(msg));
+    }
+    if (str[str.size() - 1] != ')')
+    {
+        std::string msg("cannot parse ");
+        msg += typeid(T).name();
+        msg += ": string '";
+        msg += str;
+        msg += "' does not end with closed parenthesis";
+        throw std::runtime_error(std::move(msg));
+    }
+    inner = str.substr(pos+1, str.size() - pos - 2);
+    return T::parseStyle(str.substr(0, pos));
 }
 
 // Parse a list of numbers of the given size
@@ -154,37 +150,49 @@ struct NumberList
 	NumberList(const std::string& str, const std::string& what, bool has_tail=false)
         : found(0)
 	{
-		using namespace std;
-		using namespace wibble::str;
-
 		const char* start = str.c_str();
 		for (unsigned i = 0; i < SIZE; ++i)
 		{
 			// Skip colons and spaces, if any
 			while (*start && (::isspace(*start) || *start == ','))
 				++start;
-			if (!*start)
+            if (!*start)
             {
                 if (found < REQUIRED)
-                    throw wibble::exception::Consistency(string("parsing ") + what,
-                            "found " + fmt(i) + " values instead of " + fmt(SIZE));
+                {
+                    std::stringstream ss;
+                    ss << "cannot parse " << what << ": found " << i << " values instead of " << SIZE;
+                    throw std::runtime_error(ss.str());
+                }
                 else
                     break;
             }
 
-			// Parse the number
-			char* endptr;
-			vals[i] = strtol(start, &endptr, 10);
-			if (endptr == start)
-				throw wibble::exception::Consistency(string("parsing ") + what,
-					"expected a number, but found \"" + str.substr(start - str.c_str()) + "\"");
+            // Parse the number
+            char* endptr;
+            vals[i] = ::strtol(start, &endptr, 10);
+            if (endptr == start)
+            {
+                std::string msg("cannot parse ");
+                msg += what;
+                msg += ": expected a number, but found \"";
+                msg += start;
+                msg += '"';
+                throw std::runtime_error(std::move(msg));
+            }
 
-			start = endptr;
+            start = endptr;
             ++found;
-		}
-		if (!has_tail && *start)
-			throw wibble::exception::Consistency(string("parsing ") + what,
-				"found trailing characters at the end: \"" + str.substr(start - str.c_str()) + "\"");
+        }
+        if (!has_tail && *start)
+        {
+            std::string msg("cannot parse ");
+            msg += what;
+            msg += ": found trailing characters at the end: \"";
+            msg += start;
+            msg += '"';
+            throw std::runtime_error(std::move(msg));
+        }
 		else if (has_tail && *start)
 		{
 			// Skip colons and spaces, if any
@@ -195,89 +203,11 @@ struct NumberList
 	}
 };
 
-// Parse a list of floats of the given size
-template<int SIZE>
-struct FloatList
-{
-	float vals[SIZE];
-
-	unsigned size() const { return SIZE; }
-
-	FloatList(const std::string& str, const std::string& what)
-	{
-		using namespace std;
-		using namespace wibble::str;
-
-		const char* start = str.c_str();
-		for (unsigned i = 0; i < SIZE; ++i)
-		{
-			// Skip colons and spaces, if any
-			while (*start && (::isspace(*start) || *start == ','))
-				++start;
-			if (!*start)
-				throw wibble::exception::Consistency(string("parsing ") + what,
-					"found " + fmt(i) + " values instead of " + fmt(SIZE));
-
-			// Parse the number
-			char* endptr;
-			vals[i] = strtof(start, &endptr);
-			if (endptr == start)
-				throw wibble::exception::Consistency(string("parsing ") + what,
-					"expected a number, but found \"" + str.substr(start - str.c_str()) + "\"");
-
-			start = endptr;
-		}
-		if (*start)
-			throw wibble::exception::Consistency(string("parsing ") + what,
-				"found trailing characters at the end: \"" + str.substr(start - str.c_str()) + "\"");
-	}
-};
-
-
-// Parse a list of floats of the given size
-template<int SIZE> struct DoubleList
-{
-	double vals[SIZE];
-
-	unsigned size() const { return SIZE; }
-
-	DoubleList(const std::string& str, const std::string& what)
-	{
-		using namespace std;
-		using namespace wibble::str;
-
-		const char* start = str.c_str();
-		for (unsigned i = 0; i < SIZE; ++i)
-		{
-			// Skip colons and spaces, if any
-			while (*start && (::isspace(*start) || *start == ','))
-				++start;
-			if (!*start)
-				throw wibble::exception::Consistency(string("parsing ") + what,
-					"found " + fmt(i) + " values instead of " + fmt(SIZE));
-
-			// Parse the number
-			char* endptr;
-			vals[i] = strtof(start, &endptr);
-			if (endptr == start)
-				throw wibble::exception::Consistency(string("parsing ") + what,
-					"expected a number, but found \"" + str.substr(start - str.c_str()) + "\"");
-
-			start = endptr;
-		}
-		if (*start)
-			throw wibble::exception::Consistency(string("parsing ") + what,
-				"found trailing characters at the end: \"" + str.substr(start - str.c_str()) + "\"");
-	}
-};
-
-/* functions to split strings */
+// functions to split strings
 void split(const std::string& str, std::set<std::string>& result, const std::string& delimiters = ",");
 void split(const std::string& str, std::vector<std::string>& result, const std::string& delimiters = ",");
 
-
 }
 }
 
-// vim:set ts=4 sw=4:
 #endif
