cb40267e05e2fd84202f0d2ad8504a1adb24e836
Enrico Zini <enrico@enricozini.org>
2015-12-14 11:18:38 +0100

    Refactored test runner: if tut tests are requested, do not run arki tests, and vice versa

--- arki/tests/tut-main.cpp
+++ arki/tests/tut-main.cpp
@@ -18,38 +18,27 @@ void signal_to_exception(int)
     throw std::runtime_error("killing signal catched");
 }
 
-int main(int argc,const char* argv[])
+struct TutRunner
 {
-    arki::nag::init(false, false, true);
-    arki::types::init_default_types();
     tut::reporter visi;
+    bool run_all = false;
+    string wanted_group;
+    int wanted_test = -1;
+    bool success = true;
 
-    signal(SIGSEGV,signal_to_exception);
-    signal(SIGILL,signal_to_exception);
-
-    // Run tut-based tests
-
-    if( (argc == 2 && (! strcmp ("help", argv[1]))) || argc > 3 )
+    // Setup the test runner. Returns true if program invocation has been
+    // fully handled and execution should stop after this
+    bool setup(int argc, const char* argv[])
     {
-        std::cout << "TUT example test application." << std::endl;
-        std::cout << "Usage: example [regression] | [list] | [ group] [test]" << std::endl;
-        std::cout << "       List all groups: example list" << std::endl;
-        std::cout << "       Run all tests: example regression" << std::endl;
-        std::cout << "       Run one group: example std::unique_ptr" << std::endl;
-        std::cout << "       Run one test: example std::unique_ptr 3" << std::endl;;
-    }
-
-    // std::cout << "\nFAILURE and EXCEPTION in these tests are FAKE ;)\n\n";
-
-    tut::runner.get().set_callback(&visi);
-
-    try
-    {
-        if( argc == 1 || (argc == 2 && std::string(argv[1]) == "regression") )
+        if ( (argc == 2 && (! strcmp ("help", argv[1]))) || argc > 3 )
         {
-            tut::runner.get().run_tests();
+            std::cout << argv[0] << " test runner." << std::endl;
+            std::cout << "Usage: " << argv[0] << " [group] [test] (runs all tests)" << std::endl;
+            std::cout << "Usage: " << argv[0] << " list (lists all test groups)" << std::endl;
+            return true;
         }
-        else if( argc == 2 && std::string(argv[1]) == "list" )
+
+        if (argc == 2 && std::string(argv[1]) == "list")
         {
             std::cout << "registered test groups:" << std::endl;
             tut::groupnames gl = tut::runner.get().list_groups();
@@ -60,96 +49,148 @@ int main(int argc,const char* argv[])
                 std::cout << "  " << *i << std::endl;
                 ++i;
             }
+            return true;
         }
-        else if( argc == 2 && std::string(argv[1]) != "regression" )
-        {
-            tut::runner.get().run_tests(argv[1]);
-        }
-        else if( argc == 3 )
-        {
-            tut::runner.get().run_test(argv[1],::atoi(argv[2]));
-        }
+
+        if (argc > 1)
+            wanted_group = argv[1];
+        else
+            run_all = true;
+        if (argc > 2)
+            wanted_test = ::atoi(argv[2]);
+
+        tut::runner.get().set_callback(&visi);
+        return false;
     }
-    catch( const std::exception& ex )
+
+    void run()
     {
-        std::cerr << "tut raised exception: " << ex.what() << std::endl;
+        try
+        {
+            if (wanted_group.empty())
+                tut::runner.get().run_tests();
+            else if (wanted_test == -1)
+                tut::runner.get().run_tests(wanted_group);
+            else
+                tut::runner.get().run_test(wanted_group, wanted_test);
+        }
+        catch( const std::exception& ex )
+        {
+            std::cerr << "tut raised exception: " << ex.what() << std::endl;
+        }
+
+        success = !visi.failures_count && !visi.exceptions_count;
     }
+};
 
-
-    // Run arki::tests based tests
-
-    auto& tests = arki::utils::tests::TestRegistry::get();
-
+struct ArkiRunner
+{
     arki::utils::tests::SimpleTestController controller;
-
-    if (const char* whitelist = getenv("TEST_WHITELIST"))
-        controller.whitelist = whitelist;
-
-    if (const char* blacklist = getenv("TEST_BLACKLIST"))
-        controller.blacklist = blacklist;
-
-    auto all_results = tests.run_tests(controller);
-
     unsigned methods_ok = 0;
     unsigned methods_failed = 0;
     unsigned methods_skipped = 0;
     unsigned test_cases_ok = 0;
     unsigned test_cases_failed = 0;
+    bool run_all = true;
+    bool success = true;
 
-    for (const auto& tc_res: all_results)
+    bool setup()
     {
-        if (!tc_res.fail_setup.empty())
+        if (const char* whitelist = getenv("TEST_WHITELIST"))
         {
-            fprintf(stderr, "%s: %s\n", tc_res.test_case.c_str(), tc_res.fail_setup.c_str());
-            ++test_cases_failed;
-        } else {
-            if (!tc_res.fail_teardown.empty())
-            {
-                fprintf(stderr, "%s: %s\n", tc_res.test_case.c_str(), tc_res.fail_teardown.c_str());
-                ++test_cases_failed;
-            }
-            else
-                ++test_cases_ok;
+            run_all = false;
+            controller.whitelist = whitelist;
+        }
 
-            for (const auto& tm_res: tc_res.methods)
+        if (const char* blacklist = getenv("TEST_BLACKLIST"))
+        {
+            run_all = false;
+            controller.blacklist = blacklist;
+        }
+        return false;
+    }
+
+    void run()
+    {
+        auto& tests = arki::utils::tests::TestRegistry::get();
+        auto all_results = tests.run_tests(controller);
+
+        for (const auto& tc_res: all_results)
+        {
+            if (!tc_res.fail_setup.empty())
             {
-                if (tm_res.skipped)
-                    ++methods_skipped;
-                else if (tm_res.is_success())
-                    ++methods_ok;
-                else
+                fprintf(stderr, "%s: %s\n", tc_res.test_case.c_str(), tc_res.fail_setup.c_str());
+                ++test_cases_failed;
+            } else {
+                if (!tc_res.fail_teardown.empty())
                 {
-                    fprintf(stderr, "\n");
-                    if (tm_res.exception_typeid.empty())
-                        fprintf(stderr, "%s.%s: %s\n", tm_res.test_case.c_str(), tm_res.test_method.c_str(), tm_res.error_message.c_str());
+                    fprintf(stderr, "%s: %s\n", tc_res.test_case.c_str(), tc_res.fail_teardown.c_str());
+                    ++test_cases_failed;
+                }
+                else
+                    ++test_cases_ok;
+
+                for (const auto& tm_res: tc_res.methods)
+                {
+                    if (tm_res.skipped)
+                        ++methods_skipped;
+                    else if (tm_res.is_success())
+                        ++methods_ok;
                     else
-                        fprintf(stderr, "%s.%s:[%s] %s\n", tm_res.test_case.c_str(), tm_res.test_method.c_str(), tm_res.exception_typeid.c_str(), tm_res.error_message.c_str());
-                    for (const auto& frame : tm_res.error_stack)
-                        fprintf(stderr, "  %s", frame.format().c_str());
-                    ++methods_failed;
+                    {
+                        fprintf(stderr, "\n");
+                        if (tm_res.exception_typeid.empty())
+                            fprintf(stderr, "%s.%s: %s\n", tm_res.test_case.c_str(), tm_res.test_method.c_str(), tm_res.error_message.c_str());
+                        else
+                            fprintf(stderr, "%s.%s:[%s] %s\n", tm_res.test_case.c_str(), tm_res.test_method.c_str(), tm_res.exception_typeid.c_str(), tm_res.error_message.c_str());
+                        for (const auto& frame : tm_res.error_stack)
+                            fprintf(stderr, "  %s", frame.format().c_str());
+                        ++methods_failed;
+                    }
                 }
             }
         }
+
+        if (test_cases_failed)
+        {
+            success = false;
+            fprintf(stderr, "\n%u/%u test cases had issues initializing or cleaning up\n",
+                    test_cases_failed, test_cases_ok + test_cases_failed);
+        }
+
+        if (methods_failed)
+        {
+            success = false;
+            fprintf(stderr, "\n%u/%u tests failed\n", methods_failed, methods_ok + methods_failed);
+        }
+        else
+            fprintf(stderr, "%u tests succeeded\n", methods_ok);
     }
+};
 
-    bool success = true;
+int main(int argc,const char* argv[])
+{
+    arki::nag::init(false, false, true);
+    arki::types::init_default_types();
 
-    if (test_cases_failed)
-    {
-        success = false;
-        fprintf(stderr, "\n%u/%u test cases had issues initializing or cleaning up\n",
-                test_cases_failed, test_cases_ok + test_cases_failed);
-    }
+    signal(SIGSEGV,signal_to_exception);
+    signal(SIGILL,signal_to_exception);
 
-    if (methods_failed)
-    {
-        success = false;
-        fprintf(stderr, "\n%u/%u tests failed\n", methods_failed, methods_ok + methods_failed);
-    }
-    else
-        fprintf(stderr, "%u tests succeeded\n", methods_ok);
+    // Run tut-based tests
+    TutRunner tut_runner;
+    if (tut_runner.setup(argc, argv))
+        return 0;
 
-    if (!success) return 1;
-    if (visi.failures_count || visi.exceptions_count) return 1;
+    // Run arki::tests based tests
+    ArkiRunner arki_runner;
+    if (arki_runner.setup())
+        return 0;
+
+    if (arki_runner.run_all)
+        tut_runner.run();
+    if (tut_runner.run_all)
+        arki_runner.run();
+
+    if (!arki_runner.success || !tut_runner.success) return 1;
     return 0;
 }
