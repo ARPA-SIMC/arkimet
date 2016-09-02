752cbca2416ada9edb514812c7241cde141459c8
Enrico Zini <enrico@enricozini.org>
2016-09-02 15:11:21 +0200

    Also check the behaviour of delete age

--- arki/dataset/segmented-test.cc
+++ arki/dataset/segmented-test.cc
@@ -364,6 +364,36 @@ add_method("archive_age", [](Fixture& f) {
     }
 });
 
+// Ensure that the segment is not deleted if it can still accept new data
+// after the archive cutoff (regardless of the data currently in the
+// segment)
+add_method("delete_age", [](Fixture& f) {
+    // TZ=UTC date --date="2008-01-01 00:00:00" +%s
+    time_t start2008 = 1199145600;
+
+    f.cfg.setValue("delete age", "1");
+    f.cfg.setValue("step", "yearly");
+
+    // Import a file with a known reftime
+    // Reftime: 2007-07-08T13:00:00Z
+    metadata::Collection mds("inbound/test.grib1");
+    dataset::Writer::AcquireResult res = wcallchecked(f.makeSegmentedWriter()->acquire(mds[0]));
+    wassert(actual(res) == dataset::Writer::ACQ_OK);
+    wassert(actual(f.makeSegmentedChecker().get()).check_clean(true));
+
+    {
+        TestOverrideCurrentDateForMaintenance o(start2008);
+        wassert(actual(f.makeSegmentedChecker().get()).repack_clean(false));
+    }
+
+    {
+        TestOverrideCurrentDateForMaintenance o(start2008 + 86400);
+        ReporterExpected e;
+        e.deleted.emplace_back("testds", "20/2007.grib");
+        wassert(actual(f.makeSegmentedChecker().get()).repack(e, false));
+    }
+});
+
 
 }
 }
