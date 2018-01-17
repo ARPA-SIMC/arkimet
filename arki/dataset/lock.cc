22ab7b8036fa7dd634a547d5aa58c9b53cb3f4a0
Enrico Zini <enrico@enricozini.org>
2018-01-17 20:16:40 +0100

    Implemented lock files for dataset segments

--- arki/dataset/lock.cc
+++ arki/dataset/lock.cc
@@ -8,8 +8,8 @@ using namespace arki::utils;
 namespace arki {
 namespace dataset {
 
-DatasetReadLock::DatasetReadLock(const LocalConfig& config)
-    : lockfile(str::joinpath(config.path, "lock"), O_RDWR | O_CREAT, 0777), lock_policy(config.lock_policy)
+ReadLock::ReadLock(const std::string& pathname, const core::lock::Policy* lock_policy)
+    : lockfile(pathname, O_RDWR | O_CREAT, 0777), lock_policy(lock_policy)
 {
     ds_lock.l_type = F_RDLCK;
     ds_lock.l_whence = SEEK_SET;
@@ -20,7 +20,7 @@ DatasetReadLock::DatasetReadLock(const LocalConfig& config)
     lock_policy->setlkw(lockfile, ds_lock);
 }
 
-DatasetReadLock::~DatasetReadLock()
+ReadLock::~ReadLock()
 {
     ds_lock.l_type = F_UNLCK;
     lock_policy->setlk(lockfile, ds_lock);
@@ -30,9 +30,9 @@ namespace {
 
 struct TemporaryWriteLock : public core::Lock
 {
-    std::shared_ptr<DatasetReadLock> parent;
+    std::shared_ptr<ReadLock> parent;
 
-    TemporaryWriteLock(std::shared_ptr<DatasetReadLock> parent)
+    TemporaryWriteLock(std::shared_ptr<ReadLock> parent)
         : parent(parent)
     {
         parent->ds_lock.l_type = F_WRLCK;
@@ -48,19 +48,19 @@ struct TemporaryWriteLock : public core::Lock
 
 }
 
-std::shared_ptr<core::Lock> DatasetReadLock::write_lock()
+std::shared_ptr<core::Lock> ReadLock::write_lock()
 {
     if (!current_write_lock.expired())
         return current_write_lock.lock();
 
-    std::shared_ptr<core::Lock> res(new TemporaryWriteLock(std::dynamic_pointer_cast<DatasetReadLock>(this->shared_from_this())));
+    std::shared_ptr<core::Lock> res(new TemporaryWriteLock(std::dynamic_pointer_cast<ReadLock>(this->shared_from_this())));
     current_write_lock = res;
     return res;
 }
 
 
-DatasetWriteLock::DatasetWriteLock(const LocalConfig& config)
-    : lockfile(str::joinpath(config.path, "lock"), O_RDWR | O_CREAT, 0777), lock_policy(config.lock_policy)
+WriteLock::WriteLock(const std::string& pathname, const core::lock::Policy* lock_policy)
+    : lockfile(pathname, O_RDWR | O_CREAT, 0777), lock_policy(lock_policy)
 {
     ds_lock.l_type = F_WRLCK;
     ds_lock.l_whence = SEEK_SET;
@@ -71,10 +71,31 @@ DatasetWriteLock::DatasetWriteLock(const LocalConfig& config)
     lock_policy->setlkw(lockfile, ds_lock);
 }
 
-DatasetWriteLock::~DatasetWriteLock()
+WriteLock::~WriteLock()
 {
     ds_lock.l_type = F_UNLCK;
     lock_policy->setlk(lockfile, ds_lock);
+}
+
+
+DatasetReadLock::DatasetReadLock(const LocalConfig& config)
+    : ReadLock(str::joinpath(config.path, "lock"), config.lock_policy)
+{
+}
+
+SegmentReadLock::SegmentReadLock(const LocalConfig& config, const std::string& relpath)
+    : ReadLock(str::joinpath(config.path, relpath, ".lock"), config.lock_policy)
+{
+}
+
+DatasetWriteLock::DatasetWriteLock(const LocalConfig& config)
+    : WriteLock(str::joinpath(config.path, "lock"), config.lock_policy)
+{
+}
+
+SegmentWriteLock::SegmentWriteLock(const LocalConfig& config, const std::string& relpath)
+    : WriteLock(str::joinpath(config.path, relpath, ".lock"), config.lock_policy)
+{
 }
 
 }
--- arki/dataset/lock.h
+++ arki/dataset/lock.h
@@ -8,15 +8,15 @@ namespace arki {
 namespace dataset {
 struct LocalConfig;
 
-struct DatasetReadLock : public core::Lock
+struct ReadLock : public core::Lock
 {
     arki::core::File lockfile;
     const core::lock::Policy* lock_policy;
     arki::core::FLock ds_lock;
     std::weak_ptr<core::Lock> current_write_lock;
 
-    DatasetReadLock(const LocalConfig& config);
-    ~DatasetReadLock();
+    ReadLock(const std::string& pathname, const core::lock::Policy* lock_policy);
+    ~ReadLock();
 
     /**
      * Escalate a read lock to a write lock as long as the resulting lock is in
@@ -25,15 +25,39 @@ struct DatasetReadLock : public core::Lock
     std::shared_ptr<core::Lock> write_lock();
 };
 
-struct DatasetWriteLock : public core::Lock
+
+struct WriteLock : public core::Lock
 {
     arki::core::File lockfile;
     const core::lock::Policy* lock_policy;
     arki::core::FLock ds_lock;
 
-    DatasetWriteLock(const LocalConfig& config);
-    ~DatasetWriteLock();
+    WriteLock(const std::string& pathname, const core::lock::Policy* lock_policy);
+    ~WriteLock();
 };
+
+
+
+struct DatasetReadLock : public ReadLock
+{
+    DatasetReadLock(const LocalConfig& config);
+};
+
+struct DatasetWriteLock : public WriteLock
+{
+    DatasetWriteLock(const LocalConfig& config);
+};
+
+struct SegmentReadLock : public ReadLock
+{
+    SegmentReadLock(const LocalConfig& config, const std::string& relpath);
+};
+
+struct SegmentWriteLock : public WriteLock
+{
+    SegmentWriteLock(const LocalConfig& config, const std::string& relpath);
+};
+
 
 }
 }
