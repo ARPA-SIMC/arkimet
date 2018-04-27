141a5e51281cb26ba5a3b102ec032f7c36875756
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2018-04-27 16:21:32 +0200

    Travis CI

---
+++ .travis-build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -ex
+
+image=$1
+
+if [[ $image =~ ^centos: ]]
+then
+    pkgcmd="yum"
+    builddep="yum-builddep"
+    sed -i '/^tsflags=/d' /etc/yum.conf
+    yum install -y epel-release
+    yum install -y @buildsys-build
+    yum install -y yum-utils
+    yum install -y yum-plugin-copr
+    yum install -y git
+    yum copr enable -y simc/stable
+elif [[ $image =~ ^fedora: ]]
+then
+    pkgcmd="dnf"
+    builddep="dnf builddep"
+    sed -i '/^tsflags=/d' /etc/dnf/dnf.conf
+    dnf install -y @buildsys-build
+    dnf install -y 'dnf-command(builddep)'
+    dnf install -y git
+    dnf copr enable -y simc/stable
+fi
+
+$builddep -y fedora/SPECS/arkimet.spec
+
+if [[ $image =~ ^fedora: || $image =~ ^centos: ]]
+then
+    pkgname=arkimet-$(git describe --abbrev=0 --tags --match='v*' | sed -e 's,^v,,g')
+    mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+    cp fedora/SPECS/arkimet.spec ~/rpmbuild/SPECS/arkimet.spec
+    cp fedora/SOURCES/* ~/rpmbuild/SOURCES/
+    git archive --prefix=$pkgname/ --format=tar HEAD | gzip -c > ~/rpmbuild/SOURCES/$pkgname.tar.gz
+    rpmbuild -ba ~/rpmbuild/SPECS/arkimet.spec
+    find ~/rpmbuild/{RPMS,SRPMS}/ -name "${pkgname}*rpm" -exec cp -v {} . \;
+    # TODO upload ${pkgname}*.rpm to github release on deploy stage
+else
+    autoreconf -ifv
+    ./configure
+    make check
+fi
