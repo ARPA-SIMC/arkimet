e2ad509f86a1d7b9e477e1d20e10a01a1bf675e7
Enrico Zini <enrico@enricozini.org>
2018-10-09 17:47:36 +0200

    Preserve the seqfile timestamp, not the dir timestamp

--- arki/segment/dir.cc
+++ arki/segment/dir.cc
@@ -664,8 +664,8 @@ void BaseChecker<Segment>::test_truncate(size_t offset)
 template<typename Segment>
 void BaseChecker<Segment>::test_make_hole(metadata::Collection& mds, unsigned hole_size, unsigned data_idx)
 {
-    utils::files::PreserveFileTimes pf(this->segment().abspath);
     SequenceFile seqfile(this->segment().abspath);
+    utils::files::PreserveFileTimes pf(seqfile.name());
     seqfile.open();
     size_t pos = seqfile.read_sequence();
     if (data_idx >= mds.size())
