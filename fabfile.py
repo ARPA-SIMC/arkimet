8d0ebaf5adc5091ee5e11226b70ccf2d8275fe26
Enrico Zini <enrico@enricozini.org>
2018-12-20 17:15:30 +0100

    Refactored fabfile, using a generic method to call configure, and adding ventotto

--- fabfile.py
+++ fabfile.py
@@ -2,106 +2,63 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
-from fabric.api import local, run, sudo, cd, env, hosts, shell_env
-from fabric.contrib.files import exists
+from fabric.api import local, run, cd, env, hosts, put
+from io import BytesIO
 import git
 import re
 from six.moves import shlex_quote
 
-env.hosts = ["venti", "ventiquattro", "sette"]
+env.hosts = ["venti", "ventiquattro", "ventotto", "sette"]
 env.use_ssh_config = True
-
-_common_configure_args = [
-        "--build=x86_64-redhat-linux-gnu",
-        "--host=x86_64-redhat-linux-gnu",
-        "--program-prefix=",
-        "--prefix=/usr",
-        "--exec-prefix=/usr",
-        "--bindir=/usr/bin",
-        "--sbindir=/usr/sbin",
-        "--sysconfdir=/etc",
-        "--datadir=/usr/share",
-        "--includedir=/usr/include",
-        "--libdir=/usr/lib64",
-        "--libexecdir=/usr/libexec",
-        "--localstatedir=/var",
-        "--sharedstatedir=/var/lib",
-        "--mandir=/usr/share/man",
-        "--infodir=/usr/share/info",
-        "--disable-dependency-tracking",
-]
-CONFIGURE_ARGS = {
-    "venti": _common_configure_args + ["--enable-arpae-tests"],
-    "ventiquattro": _common_configure_args + ["--enable-arpae-tests"],
-    "sette": _common_configure_args,
-}
-
-_common_cxxflags = "-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong"\
-                   " --param=ssp-buffer-size=4 -grecord-gcc-switches  -m64 -mtune=generic"
-CXXFLAGS = {
-    "venti": _common_cxxflags,
-    "ventiquattro": _common_cxxflags + "-Werror=format-security -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1",
-    "sette": _common_cxxflags,
-}
-
-_common_ldflags = "-Wl,-z,relro"
-LDFLAGS = {
-    "venti": _common_ldflags,
-    "ventiquattro": _common_ldflags + " -specs=/usr/lib/rpm/redhat/redhat-hardened-ld",
-    "sette": _common_ldflags,
-}
-
-TEST_WORKAREA = {
-    "venti": "/tmp",
-    "ventiquattro": "/tmp",
-    "sette": "/home/makerpm/testarea",
-}
 
 
 def cmd(*args):
     return " ".join(shlex_quote(a) for a in args)
 
 
-def _test(name):
-    cxxflags = CXXFLAGS[name]
-    ldflags = LDFLAGS[name]
+def configure():
+    res = run(cmd("rpm", "--eval", "%configure --enable-arpae-tests"))
+    put(BytesIO(b"\n".join(res.stdout.splitlines())), "rpm-config")
+    run(cmd("chmod", "0755", "rpm-config"))
+    run(cmd("./rpm-config"))
 
+
+def run_test(host):
     repo = git.Repo()
-    remote = repo.remote(name)
+    remote = repo.remote(host)
     push_url = remote.config_reader.get("url")
     remote_dir = re.sub(r"^ssh://[^/]+", "", push_url)
-    configure_cmd = ["./configure"] + CONFIGURE_ARGS[name] + [
-        "CFLAGS=" + cxxflags,
-        "CXXFLAGS=" + cxxflags,
-        "LDFLAGS=" + ldflags,
-    ]
-    test_workarea = TEST_WORKAREA[name]
 
-    local(cmd("git", "push", "--force", name, "HEAD"))
+    local(cmd("git", "push", host, "HEAD", "--force"))
     with cd(remote_dir):
         run(cmd("git", "reset", "--hard"))
         run(cmd("git", "clean", "-fx"))
-        run(cmd("git", "checkout", "-B", "test_" + name, repo.head.commit.hexsha))
+        run(cmd("git", "checkout", "-B", "test_" + host, repo.head.commit.hexsha))
+        run(cmd("git", "clean", "-fx"))
         run(cmd("autoreconf", "-if"))
-        run(cmd(*configure_cmd))
-        run(cmd("make"))
-        with shell_env(TMPDIR=test_workarea):
-            run(cmd("make", "check", "TEST_VERBOSE=1"))
+        configure()
+        run(cmd("make", "-j2"))
+        run(cmd("make", "check", "-j2", "TEST_VERBOSE=1"))
 
 
 @hosts("venti")
 def test_venti():
-    _test("venti")
+    run_test("venti")
 
 
 @hosts("ventiquattro")
 def test_ventiquattro():
-    _test("ventiquattro")
+    run_test("ventiquattro")
 
 
-@hosts("makerpm@sette")
+@hosts("ventotto")
+def test_ventotto():
+    run_test("ventotto")
+
+
+@hosts("sette")
 def test_sette():
-    _test("sette")
+    run_test("sette")
 
 
 def test():
