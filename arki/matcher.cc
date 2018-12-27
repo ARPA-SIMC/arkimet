efcb337a2bf6b72c1c7d05c7758215522ce18489
Enrico Zini <enrico@enricozini.org>
2018-12-27 16:16:40 +0100

    Reproduced in test suite

--- arki/matcher.cc
+++ arki/matcher.cc
@@ -200,6 +200,7 @@ bool AND::matchItemSet(const ItemSet& md) const
     for (const auto& i: components)
     {
         const Type* item = md.get(i.first);
+        fprintf(stderr, "ITEM %d: %p\n", (int)i.first, item);
         if (!i.second) return false;
         if (!i.second->matchItem(*item)) return false;
     }
