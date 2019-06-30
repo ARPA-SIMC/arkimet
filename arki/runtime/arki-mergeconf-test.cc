112782c6843a760f67c6779561cbc7ef7f38f70a
Enrico Zini <enrico@enricozini.org>
2019-06-30 12:25:34 +0200

    Moved arki-mergeconf tests to python. refs: #177

--- arki/runtime/arki-mergeconf-test.cc
+++ arki/runtime/arki-mergeconf-test.cc
@@ -1,6 +1,5 @@
 #include "arki/tests/tests.h"
 #include "arki/runtime/tests.h"
-#include "arki/tests/daemon.h"
 #include "arki-mergeconf.h"
 
 namespace {
@@ -19,65 +18,7 @@ Tests test("arki_runtime_arkimergeconf");
 
 void Tests::register_tests() {
 
-add_method("http", [] {
-    using runtime::tests::run_cmdline;
-
-    Daemon server({"arki/dataset/http-redirect-daemon"});
-    int port;
-    sscanf(server.daemon_start_string.c_str(), "OK %d\n", &port);
-    char url[512];
-    snprintf(url, 512, "http://localhost:%d", port);
-
-    {
-        runtime::tests::CatchOutput co;
-        int res = run_cmdline<runtime::ArkiMergeconf>({ "arki-mergeconf", url });
-        wassert(actual(res) == 0);
-        wassert(actual_file(co.file_stderr.name()).empty());
-        wassert(actual_file(co.file_stdout.name()).contents_equal({
-            "[error]",
-            "name = error",
-            "path = http://foo.bar/foo/dataset/error",
-            "type = remote",
-            "",
-            "[test200]",
-            "name = test200",
-            "path = http://foo.bar/foo/dataset/test200",
-            "type = remote",
-            "",
-            "[test80]",
-            "name = test80",
-            "path = http://foo.bar/foo/dataset/test80",
-            "type = remote",
-        }));
-    }
-});
-
-add_method("ignore_system_datasets", [] {
-    using runtime::tests::run_cmdline;
-
-    Daemon server({"arki/dataset/http-redirect-daemon"});
-    int port;
-    sscanf(server.daemon_start_string.c_str(), "OK %d\n", &port);
-    char url[512];
-    snprintf(url, 512, "http://localhost:%d", port);
-
-    {
-        runtime::tests::CatchOutput co;
-        int res = run_cmdline<runtime::ArkiMergeconf>({ "arki-mergeconf", "--ignore-system-datasets", url });
-        wassert(actual(res) == 0);
-        wassert(actual_file(co.file_stderr.name()).contents_equal({}));
-        wassert(actual_file(co.file_stdout.name()).contents_equal({
-            "[test200]",
-            "name = test200",
-            "path = http://foo.bar/foo/dataset/test200",
-            "type = remote",
-            "",
-            "[test80]",
-            "name = test80",
-            "path = http://foo.bar/foo/dataset/test80",
-            "type = remote",
-        }));
-    }
+add_method("empty", [] {
 });
 
 }
---
+++ python/tests/test_arki_mergeconf.py
@@ -0,0 +1,90 @@
+import unittest
+import subprocess
+from contextlib import contextmanager
+import arkimet as arki
+import os
+import tempfile
+
+
+@contextmanager
+def daemon(*cmd):
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, text=True)
+    try:
+        init = proc.stdout.readline()
+        if not init.startswith("OK "):
+            raise RuntimeError("Process startup line was {} instead of OK".format(init))
+        yield "http://localhost:" + init[3:].strip()
+    finally:
+        proc.terminate()
+        proc.wait()
+
+
+class CatchOutput:
+    def __init__(self):
+        self.stdout = None
+        self.stderr = None
+
+    @contextmanager
+    def redirect(self):
+        saved_stdout = os.dup(1)
+        saved_stderr = os.dup(2)
+        with tempfile.TemporaryFile() as tmp_stdout:
+            with tempfile.TemporaryFile() as tmp_stderr:
+                try:
+                    os.dup2(tmp_stdout.fileno(), 1)
+                    os.dup2(tmp_stderr.fileno(), 2)
+                    yield
+                    tmp_stdout.seek(0)
+                    self.stdout = tmp_stdout.read()
+                    tmp_stderr.seek(0)
+                    self.stderr = tmp_stderr.read()
+                finally:
+                    os.dup2(saved_stdout, 1)
+                    os.dup2(saved_stderr, 2)
+
+
+class TestArkiMergeconfReader(unittest.TestCase):
+    def test_http(self):
+        with daemon(os.path.join(os.environ["TOP_SRCDIR"], "arki/dataset/http-redirect-daemon")) as url:
+            cmd = arki.ArkiMergeconf()
+            out = CatchOutput()
+            with out.redirect():
+                res = cmd.run(["arki-mergeconf", url])
+            self.assertEqual(res, 0)
+            self.assertEqual(out.stderr.decode(), "")
+            self.assertEqual(out.stdout.decode().splitlines(), [
+                "[error]",
+                "name = error",
+                "path = http://foo.bar/foo/dataset/error",
+                "type = remote",
+                "",
+                "[test200]",
+                "name = test200",
+                "path = http://foo.bar/foo/dataset/test200",
+                "type = remote",
+                "",
+                "[test80]",
+                "name = test80",
+                "path = http://foo.bar/foo/dataset/test80",
+                "type = remote",
+            ])
+
+    def test_ignore_system_datasets(self):
+        with daemon(os.path.join(os.environ["TOP_SRCDIR"], "arki/dataset/http-redirect-daemon")) as url:
+            cmd = arki.ArkiMergeconf()
+            out = CatchOutput()
+            with out.redirect():
+                res = cmd.run(["arki-mergeconf", "--ignore-system-datasets", url])
+            self.assertEqual(res, 0)
+            self.assertEqual(out.stderr.decode(), "")
+            self.assertEqual(out.stdout.decode().splitlines(), [
+                "[test200]",
+                "name = test200",
+                "path = http://foo.bar/foo/dataset/test200",
+                "type = remote",
+                "",
+                "[test80]",
+                "name = test80",
+                "path = http://foo.bar/foo/dataset/test80",
+                "type = remote",
+            ])
