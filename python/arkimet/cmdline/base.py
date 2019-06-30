b459722ccf72282bec72a8441a5016bada538514
Enrico Zini <enrico@enricozini.org>
2019-06-30 19:43:12 +0200

    More common infrastructure for testing python command line, and go back to test the command line tool. refs: #177

---
+++ python/arkimet/cmdline/base.py
@@ -0,0 +1,67 @@
+import argparse
+import logging
+import sys
+
+
+class Fail(Exception):
+    pass
+
+
+class Exit(Exception):
+    pass
+
+
+class App:
+    NAME = None
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description=self.get_description)
+        self.parser.add_argument("--verbose", "-v", action="store_true",
+                                 help="verbose output")
+        self.parser.add_argument("--debug", action="store_true",
+                                 help="debug output")
+        self.parser.add_argument("--version", action="store_true",
+                                 help="print the program version and exit")
+        self.log = logging.getLogger(self.get_name())
+        self.args = None
+
+    def get_description(self):
+        return self.__doc__
+
+    def get_name(self):
+        name = self.NAME
+        if name is not None:
+            return name
+        return "arki-" + self.__class__.__name__[4:].lower()
+
+    def parse_args(self, args=None):
+        self.args = self.parser.parse_args(args=args)
+
+    def run(self):
+        if self.args.version:
+            import arkimet
+            print(arkimet.get_version())
+            raise Exit()
+
+        log_format = "%(levelname)s %(message)s"
+        level = logging.WARN
+        if self.args.debug:
+            level = logging.DEBUG
+        elif self.args.verbose:
+            level = logging.INFO
+        logging.basicConfig(level=level, stream=sys.stderr, format=log_format)
+
+    @classmethod
+    def main(cls, args=None):
+        try:
+            cmd = cls()
+            cmd.parse_args(args)
+            cmd.run()
+        except Exit as e:
+            if not e.args:
+                return
+            else:
+                return e.args[0]
+        except Fail as e:
+            print(e, file=sys.stderr)
+            return 1
---
+++ python/arkimet/cmdline/mergeconf.py
@@ -0,0 +1,53 @@
+import arkimet as arki
+from arkimet.cmdline.base import App
+import sys
+import logging
+
+log = logging.getLogger("arki-mergeconf")
+
+
+class Mergeconf(App):
+    """
+    Read dataset configuration from the given directories or config files,
+    merge them and output the merged config file to standard output
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.parser.add_argument("-o", "--output", metavar="file",
+                                 help="write the output to the given file instead of standard output")
+        self.parser.add_argument("--extra", action="store_true",
+                                 help="extract extra information from the datasets (such as bounding box)"
+                                      " and include it in the configuration")
+        self.parser.add_argument("--ignore-system-datasets", action="store_true",
+                                 help="ignore error and duplicates datasets")
+        self.parser.add_argument("--restrict", metavar="names", action="store",
+                                 help="restrict operations to only those datasets that allow one of the given"
+                                      " (comma separated) names")
+        self.parser.add_argument("-C", "--config", metavar="file", action="append",
+                                 help="merge configuration from the given file (can be given more than once)")
+        self.parser.add_argument("sources", nargs="*", action="store",
+                                 help="datasets, configuration files or remote urls")
+
+    def run(self):
+        super().run()
+
+        arki_mergeconf = arki.ArkiMergeconf()
+        merged = arki_mergeconf.run(
+            self.args.sources,
+            cfgfiles=self.args.config if self.args.config else None,
+            restrict=self.args.restrict,
+            ignore_system_datasets=self.args.ignore_system_datasets,
+            extra=self.args.extra,
+        )
+
+        # Output the merged configuration
+        if self.args.output:
+            with open(self.args.output, "wt") as fd:
+                merged.write(fd)
+        else:
+            merged.write(sys.stdout)
+
+
+if __name__ == "__main__":
+    sys.exit(Mergeconf.main())
