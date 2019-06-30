dbb1757aad9c56fa3127398fb0085d73f0d75472
Enrico Zini <enrico@enricozini.org>
2019-06-30 12:49:30 +0200

    Started bindings of cfg.Sections and cfg.Section. refs: #177

---
+++ python/cfg.cc
@@ -0,0 +1,126 @@
+#include "cfg.h"
+#include "utils/core.h"
+#include "utils/methods.h"
+#include "utils/type.h"
+#include "utils/values.h"
+#include "common.h"
+
+using namespace arki::python;
+
+extern "C" {
+
+PyTypeObject* arkipy_cfgSections_Type = nullptr;
+PyTypeObject* arkipy_cfgSection_Type = nullptr;
+
+}
+
+
+namespace {
+
+#if 0
+struct run_ : public MethKwargs<run_, arkipy_cfgSections>
+{
+    constexpr static const char* name = "run";
+    constexpr static const char* signature = "";
+    constexpr static const char* returns = "int";
+    constexpr static const char* summary = "run arki-mergeconf";
+    constexpr static const char* doc = nullptr;
+
+    static PyObject* run(Impl* self, PyObject* args, PyObject* kw)
+    {
+        static const char* kwlist[] = { "args", nullptr };
+        PyObject* pyargs = nullptr;
+        if (!PyArg_ParseTupleAndKeywords(args, kw, "O", const_cast<char**>(kwlist), &pyargs))
+            return nullptr;
+
+        if (!PySequence_Check(pyargs))
+        {
+            PyErr_SetString(PyExc_TypeError, "args argument must be a sequence of strings");
+            throw PythonException();
+        }
+
+        // Unpack a sequence of strings into argc/argv
+        unsigned argc = PySequence_Size(pyargs);
+        std::vector<const char*> argv;
+        argv.reserve(argc + 1);
+        for (unsigned i = 0; i < argc; ++i)
+        {
+            pyo_unique_ptr o(throw_ifnull(PySequence_ITEM(pyargs, i)));
+            argv.emplace_back(from_python<const char*>(o));
+        }
+        argv.emplace_back(nullptr);
+
+        try {
+            int res;
+            {
+                ReleaseGIL rg;
+                res = self->arki_mergeconf->run(argc, argv.data());
+            }
+            return throw_ifnull(PyLong_FromLong(res));
+        } ARKI_CATCH_RETURN_PYO
+    }
+};
+#endif
+
+
+struct SectionsDef : public Type<SectionsDef, arkipy_cfgSections>
+{
+    constexpr static const char* name = "Sections";
+    constexpr static const char* qual_name = "arkimet.cfg.Sections";
+    constexpr static const char* doc = R"(
+Arkimet configuration, as multiple sections of key/value options
+)";
+    GetSetters<> getsetters;
+    Methods<> methods;
+
+    static void _dealloc(Impl* self)
+    {
+        self->sections.~Sections();
+        Py_TYPE(self)->tp_free(self);
+    }
+
+    static PyObject* _str(Impl* self)
+    {
+        return PyUnicode_FromString(name);
+    }
+
+    static PyObject* _repr(Impl* self)
+    {
+        std::string res = qual_name;
+        res += " object";
+        return PyUnicode_FromString(res.c_str());
+    }
+
+    static int _init(Impl* self, PyObject* args, PyObject* kw)
+    {
+        static const char* kwlist[] = { nullptr };
+        if (!PyArg_ParseTupleAndKeywords(args, kw, "", const_cast<char**>(kwlist)))
+            return -1;
+
+        try {
+            new(&(self->sections)) arki::core::cfg::Sections();
+        } ARKI_CATCH_RETURN_INT
+
+        return 0;
+    }
+};
+
+SectionsDef* sections_def = nullptr;
+
+
+}
+
+namespace arki {
+namespace python {
+
+void register_cfg(PyObject* m)
+{
+    // TODO: add a submodule 'cfg' to m
+    sections_def = new SectionsDef;
+    sections_def->define(arkipy_cfgSections_Type, m);
+
+}
+
+}
+}
+
