8c40475297cde3c0d59e0bf09030b2171fbaa1a7
Enrico Zini <enrico@enricozini.org>
2019-06-30 13:59:54 +0200

    Started binding Section. refs: #177

--- python/tests/test_cfg.py
+++ python/tests/test_cfg.py
@@ -3,6 +3,16 @@ import arkimet as arki
 
 
 class TestCfgSections(unittest.TestCase):
-    def test_sections(self):
+    def test_empty(self):
         sections = arki.cfg.Sections()
         self.assertEqual(len(sections), 0)
+        with self.assertRaises(KeyError):
+            sections["missing"]
+
+
+class TestCfgSection(unittest.TestCase):
+    def test_empty(self):
+        section = arki.cfg.Section()
+        self.assertEqual(len(section), 0)
+        with self.assertRaises(KeyError):
+            section["missing"]
