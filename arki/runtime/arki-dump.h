caf6cb921e41c426ffeea78199d8bc75f73c5b24
Enrico Zini <enrico@enricozini.org>
2019-06-30 10:08:19 +0200

    Moved arki-dump to runtime/. refs: #177

---
+++ arki/runtime/arki-dump.h
@@ -0,0 +1,16 @@
+#ifndef ARKI_RUNTIME_ARKIDUMP_H
+#define ARKI_RUNTIME_ARKIDUMP_H
+
+namespace arki {
+namespace runtime {
+
+class ArkiDump
+{
+public:
+    int run(int argc, const char* argv[]);
+};
+
+}
+}
+
+#endif
