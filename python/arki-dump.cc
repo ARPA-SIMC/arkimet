073906123096adffb5e9bb555696f4d96e5aafb4
Enrico Zini <enrico@enricozini.org>
2019-07-09 21:28:13 +0200

    arki-dump now implemented in python. refs: #177

--- python/arki-dump.cc
+++ python/arki-dump.cc
@@ -1,12 +1,23 @@
 #include "arki/runtime/arki-dump.h"
+#include "arki/utils/sys.h"
+#include "arki/core/file.h"
+#include "arki/metadata.h"
+#include "arki/summary.h"
+#include "arki/binary.h"
+#include "arki/types.h"
+#include "arki/types/source.h"
+#include "arki/utils/geos.h"
+#include "arki/formatter.h"
 #include "arki-dump.h"
 #include "utils/core.h"
 #include "utils/methods.h"
 #include "utils/type.h"
 #include "utils/values.h"
 #include "common.h"
+#include <sstream>
 
 using namespace arki::python;
+using namespace arki::utils;
 
 extern "C" {
 
@@ -17,45 +28,281 @@ PyTypeObject* arkipy_ArkiDump_Type = nullptr;
 
 namespace {
 
-struct run_ : public MethKwargs<run_, arkipy_ArkiDump>
+std::unique_ptr<sys::NamedFileDescriptor> make_input(const std::string& input)
 {
-    constexpr static const char* name = "run";
-    constexpr static const char* signature = "";
+    if (input == "-")
+        return std::unique_ptr<sys::NamedFileDescriptor>(new arki::core::Stdin);
+
+    return std::unique_ptr<sys::NamedFileDescriptor>(new sys::File(input, O_RDONLY));
+}
+
+std::unique_ptr<sys::NamedFileDescriptor> make_output(const std::string& output)
+{
+    if (output == "-")
+        return std::unique_ptr<sys::NamedFileDescriptor>(new arki::core::Stdout);
+    else
+        return std::unique_ptr<sys::NamedFileDescriptor>(new sys::File(output, O_WRONLY | O_CREAT | O_TRUNC, 0666));
+}
+
+#ifdef HAVE_GEOS
+// Add to \a s the info from all data read from \a in
+void addToSummary(sys::NamedFileDescriptor& in, arki::Summary& s)
+{
+    arki::Metadata md;
+    arki::Summary summary;
+
+    arki::types::Bundle bundle;
+    while (bundle.read_header(in))
+    {
+        if (bundle.signature == "MD" || bundle.signature == "!D")
+        {
+            if (!bundle.read_data(in)) break;
+            arki::BinaryDecoder dec(bundle.data);
+            md.read_inner(dec, bundle.version, in.name());
+            if (md.source().style() == arki::types::Source::INLINE)
+                md.read_inline_data(in);
+            s.add(md);
+        }
+        else if (bundle.signature == "SU")
+        {
+            if (!bundle.read_data(in)) break;
+            arki::BinaryDecoder dec(bundle.data);
+            summary.read_inner(dec, bundle.version, in.name());
+            s.add(summary);
+        }
+        else if (bundle.signature == "MG")
+        {
+            if (!bundle.read_data(in)) break;
+            arki::BinaryDecoder dec(bundle.data);
+            arki::Metadata::read_group(dec, bundle.version, in.name(), [&](std::unique_ptr<arki::Metadata> md) { s.add(*md); return true; });
+        }
+        else
+            throw std::runtime_error(in.name() + ": metadata entry does not start with 'MD', '!D', 'SU', or 'MG'");
+    }
+}
+#endif
+
+struct YamlPrinter
+{
+    sys::NamedFileDescriptor& out;
+    arki::Formatter* formatter = nullptr;
+
+    YamlPrinter(sys::NamedFileDescriptor& out, bool formatted=false)
+        : out(out)
+    {
+        if (formatted)
+            formatter = arki::Formatter::create().release();
+    }
+    ~YamlPrinter()
+    {
+        delete formatter;
+    }
+
+    bool print(const arki::Metadata& md)
+    {
+        std::string res = serialize(md);
+        out.write_all_or_throw(res.data(), res.size());
+        return true;
+    }
+
+    bool print_summary(const arki::Summary& s)
+    {
+        std::string res = serialize(s);
+        out.write_all_or_throw(res.data(), res.size());
+        return true;
+    }
+
+    std::string serialize(const arki::Metadata& md)
+    {
+        std::stringstream ss;
+        md.write_yaml(ss, formatter);
+        ss << std::endl;
+        return ss.str();
+    }
+
+    std::string serialize(const arki::Summary& s)
+    {
+        std::stringstream ss;
+        s.write_yaml(ss, formatter);
+        ss << std::endl;
+        return ss.str();
+    }
+};
+
+
+struct bbox : public MethKwargs<bbox, arkipy_ArkiDump>
+{
+    constexpr static const char* name = "bbox";
+    constexpr static const char* signature = "input: str, output: str";
     constexpr static const char* returns = "int";
-    constexpr static const char* summary = "run arki-dump";
+    constexpr static const char* summary = "run arki-dump --bbox";
     constexpr static const char* doc = nullptr;
 
     static PyObject* run(Impl* self, PyObject* args, PyObject* kw)
     {
-        static const char* kwlist[] = { "args", nullptr };
-        PyObject* pyargs = nullptr;
-        if (!PyArg_ParseTupleAndKeywords(args, kw, "O", const_cast<char**>(kwlist), &pyargs))
+        static const char* kwlist[] = { "input", "output", nullptr };
+        const char* input = nullptr;
+        const char* output = nullptr;
+        if (!PyArg_ParseTupleAndKeywords(args, kw, "ss", const_cast<char**>(kwlist), &input, &output))
             return nullptr;
 
-        if (!PySequence_Check(pyargs))
-        {
-            PyErr_SetString(PyExc_TypeError, "args argument must be a sequence of strings");
-            throw PythonException();
-        }
+        try {
+            ReleaseGIL rg;
+            // Open the input file
+            auto in = make_input(input);
 
-        // Unpack a sequence of strings into argc/argv
-        unsigned argc = PySequence_Size(pyargs);
-        std::vector<const char*> argv;
-        argv.reserve(argc + 1);
-        for (unsigned i = 0; i < argc; ++i)
-        {
-            pyo_unique_ptr o(throw_ifnull(PySequence_ITEM(pyargs, i)));
-            argv.emplace_back(from_python<const char*>(o));
-        }
-        argv.emplace_back(nullptr);
+            // Read everything into a single summary
+            arki::Summary summary;
+            addToSummary(*in, summary);
+
+            // Get the bounding box
+            auto hull = summary.getConvexHull();
+
+            // Print it out
+            std::string out;
+            if (hull.get())
+                out = hull->toString();
+            else
+                out = "no bounding box could be computed.";
+
+            // Open the output file
+            std::unique_ptr<sys::NamedFileDescriptor> outfd(make_output(output));
+            outfd->write_all_or_throw(out);
+            outfd->close();
+
+            return throw_ifnull(PyLong_FromLong(0));
+        } ARKI_CATCH_RETURN_PYO
+    }
+};
+
+struct reverse_data : public MethKwargs<reverse_data, arkipy_ArkiDump>
+{
+    constexpr static const char* name = "reverse_data";
+    constexpr static const char* signature = "input: str, output: str";
+    constexpr static const char* returns = "int";
+    constexpr static const char* summary = "run arki-dump --from-yaml-data";
+    constexpr static const char* doc = nullptr;
+
+    static PyObject* run(Impl* self, PyObject* args, PyObject* kw)
+    {
+        static const char* kwlist[] = { "input", "output", nullptr };
+        const char* input = nullptr;
+        const char* output = nullptr;
+        if (!PyArg_ParseTupleAndKeywords(args, kw, "ss", const_cast<char**>(kwlist), &input, &output))
+            return nullptr;
 
         try {
-            int res;
+            ReleaseGIL rg;
+            // Open the input file
+            auto in = make_input(input);
+
+            // Open the output channel
+            std::unique_ptr<sys::NamedFileDescriptor> outfd(make_output(output));
+
+            arki::Metadata md;
+            auto reader = arki::core::LineReader::from_fd(*in);
+            while (md.readYaml(*reader, in->name()))
+                md.write(*outfd);
+
+            return throw_ifnull(PyLong_FromLong(0));
+        } ARKI_CATCH_RETURN_PYO
+    }
+};
+
+struct reverse_summary : public MethKwargs<reverse_summary, arkipy_ArkiDump>
+{
+    constexpr static const char* name = "reverse_summary";
+    constexpr static const char* signature = "input: str, output: str";
+    constexpr static const char* returns = "int";
+    constexpr static const char* summary = "run arki-dump --from-yaml-summary";
+    constexpr static const char* doc = nullptr;
+
+    static PyObject* run(Impl* self, PyObject* args, PyObject* kw)
+    {
+        static const char* kwlist[] = { "input", "output", nullptr };
+        const char* input = nullptr;
+        const char* output = nullptr;
+        if (!PyArg_ParseTupleAndKeywords(args, kw, "ss", const_cast<char**>(kwlist), &input, &output))
+            return nullptr;
+
+        try {
+            ReleaseGIL rg;
+            // Open the input file
+            auto in = make_input(input);
+
+            // Open the output channel
+            std::unique_ptr<sys::NamedFileDescriptor> outfd(make_output(output));
+
+            arki::Summary summary;
+            auto reader = arki::core::LineReader::from_fd(*in);
+            while (summary.readYaml(*reader, in->name()))
+                summary.write(*outfd, outfd->name());
+
+            return throw_ifnull(PyLong_FromLong(0));
+        } ARKI_CATCH_RETURN_PYO
+    }
+};
+
+struct dump_yaml : public MethKwargs<dump_yaml, arkipy_ArkiDump>
+{
+    constexpr static const char* name = "dump_yaml";
+    constexpr static const char* signature = "input: str, output: str";
+    constexpr static const char* returns = "int";
+    constexpr static const char* summary = "run arki-dump [--annotate]";
+    constexpr static const char* doc = nullptr;
+
+    static PyObject* run(Impl* self, PyObject* args, PyObject* kw)
+    {
+        static const char* kwlist[] = { "input", "output", "annotate", nullptr };
+        const char* input = nullptr;
+        const char* output = nullptr;
+        int annotate = 0;
+        if (!PyArg_ParseTupleAndKeywords(args, kw, "ss|p", const_cast<char**>(kwlist), &input, &output, &annotate))
+            return nullptr;
+
+        try {
+            ReleaseGIL rg;
+            // Open the input file
+            auto in = make_input(input);
+
+            // Open the output channel
+            std::unique_ptr<sys::NamedFileDescriptor> outfd(make_output(output));
+
+            YamlPrinter writer(*outfd, annotate);
+
+            arki::Metadata md;
+            arki::Summary summary;
+
+            arki::types::Bundle bundle;
+            while (bundle.read_header(*in))
             {
-                ReleaseGIL rg;
-                res = self->arki_dump->run(argc, argv.data());
+                if (bundle.signature == "MD" || bundle.signature == "!D")
+                {
+                    if (!bundle.read_data(*in)) break;
+                    arki::BinaryDecoder dec(bundle.data);
+                    md.read_inner(dec, bundle.version, in->name());
+                    if (md.source().style() == arki::types::Source::INLINE)
+                        md.read_inline_data(*in);
+                    writer.print(md);
+                }
+                else if (bundle.signature == "SU")
+                {
+                    if (!bundle.read_data(*in)) break;
+                    arki::BinaryDecoder dec(bundle.data);
+                    summary.read_inner(dec, bundle.version, in->name());
+                    writer.print_summary(summary);
+                }
+                else if (bundle.signature == "MG")
+                {
+                    if (!bundle.read_data(*in)) break;
+                    arki::BinaryDecoder dec(bundle.data);
+                    arki::Metadata::read_group(dec, bundle.version, in->name(), [&](std::unique_ptr<arki::Metadata> md) { return writer.print(*md); });
+                }
+                else
+                    throw std::runtime_error(in->name() + ": metadata entry does not start with 'MD', '!D', 'SU', or 'MG'");
             }
-            return throw_ifnull(PyLong_FromLong(res));
+
+            return throw_ifnull(PyLong_FromLong(0));
         } ARKI_CATCH_RETURN_PYO
     }
 };
@@ -69,7 +316,7 @@ struct ArkiDumpDef : public Type<ArkiDumpDef, arkipy_ArkiDump>
 arki-dump implementation
 )";
     GetSetters<> getsetters;
-    Methods<run_> methods;
+    Methods<bbox, reverse_data, reverse_summary, dump_yaml> methods;
 
     static void _dealloc(Impl* self)
     {
@@ -104,7 +351,6 @@ arki-dump implementation
 };
 
 ArkiDumpDef* arki_dump_def = nullptr;
-
 
 }
 
---
+++ python/tests/test_arki_dump.py
@@ -0,0 +1,105 @@
+import unittest
+import tempfile
+import os
+import arkimet as arki
+from arkimet.cmdline.dump import Dump
+from arkimet.test import daemon, CatchOutput
+
+
+class TestArkiDump(unittest.TestCase):
+    def test_query(self):
+        out = CatchOutput()
+        with out.redirect():
+            res = Dump.main(args=["--query", "reftime:=2019-07-07"])
+        self.assertEqual(out.stderr, b"")
+        self.assertEqual(out.stdout, b"reftime:>=2019-07-07 00:00:00,<=2019-07-07 23:59:59\n")
+        self.assertIsNone(res)
+
+    def test_aliases_local(self):
+        out = CatchOutput()
+        with out.redirect():
+            res = Dump.main(args=["--aliases"])
+        self.assertEqual(out.stderr, b"")
+        self.assertEqual(out.stdout[:7], b"[level]")
+        self.assertIsNone(res)
+
+    def test_aliases_remote(self):
+        with daemon(os.path.join(os.environ["TOP_SRCDIR"], "arki/dataset/http-aliases-daemon")) as url:
+            out = CatchOutput()
+            with out.redirect():
+                res = Dump.main(args=["--aliases", url])
+            self.assertEqual(out.stderr, b"")
+            self.assertEqual(out.stdout, b"[level]\ng00 = GRIB1,1 or GRIB2S,1,0,0\n")
+            self.assertIsNone(res)
+
+    def test_info(self):
+        import json
+        out = CatchOutput()
+        with out.redirect():
+            res = Dump.main(args=["--info"])
+        self.assertEqual(out.stderr, b"")
+        self.assertEqual(json.loads(out.stdout.decode()), arki.config())
+        self.assertIsNone(res)
+
+    def test_bbox(self):
+        out = CatchOutput()
+        with out.redirect():
+            res = Dump.main(args=["--bbox", "inbound/test.arkimet"])
+        self.assertEqual(out.stderr, b"")
+        self.assertEqual(out.stdout[:7], b"POLYGON")
+        self.assertEqual(res, 0)
+
+    def test_from_yaml_data(self):
+        out = CatchOutput()
+        with out.redirect():
+            res = Dump.main(args=["--from-yaml-data", "inbound/test.yaml"])
+        self.assertEqual(out.stderr, b"")
+        self.assertEqual(out.stdout[:2], b"MD")
+        self.assertEqual(res, 0)
+
+    def test_from_yaml_summary(self):
+        out = CatchOutput()
+        with out.redirect():
+            res = Dump.main(args=["--from-yaml-summary", "inbound/test.summary.yaml"])
+        self.assertEqual(out.stderr, b"")
+        self.assertEqual(out.stdout[:2], b"SU")
+        self.assertEqual(res, 0)
+
+    def test_from_md(self):
+        out = CatchOutput()
+        with out.redirect():
+            res = Dump.main(args=["inbound/test.arkimet"])
+        self.assertEqual(out.stderr, b"")
+        self.assertEqual(out.stdout[:8], b"Source: ")
+        self.assertNotIn(b"# Geopotential", out.stdout)
+        self.assertEqual(res, 0)
+
+    def test_from_summary(self):
+        out = CatchOutput()
+        with out.redirect():
+            res = Dump.main(args=["inbound/test.summary"])
+        self.assertEqual(out.stderr, b"")
+        self.assertEqual(out.stdout[:12], b"SummaryItem:")
+        self.assertNotIn(b"# Geopotential", out.stdout)
+        self.assertEqual(res, 0)
+
+    def test_from_md_annotated(self):
+        out = CatchOutput()
+        with out.redirect():
+            res = Dump.main(args=["--annotate", "inbound/test.arkimet"])
+        self.assertEqual(out.stderr, b"")
+        self.assertEqual(out.stdout[:8], b"Source: ")
+        self.assertIn(b"# Geopotential", out.stdout)
+        self.assertEqual(res, 0)
+
+    def test_from_summary_annotated(self):
+        out = CatchOutput()
+        with out.redirect():
+            res = Dump.main(args=["--annotate", "inbound/test.summary"])
+        self.assertEqual(out.stderr, b"")
+        self.assertEqual(out.stdout[:12], b"SummaryItem:")
+        self.assertIn(b"# Geopotential", out.stdout)
+        self.assertEqual(res, 0)
+
+        # self.parser.add_argument("-o", "--output", metavar="file",
+        #                          help="write the output to the given file instead of standard output")
