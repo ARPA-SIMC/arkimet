a6f9fc97de5b03efe319a85aeadca08e062e8931
Enrico Zini <enrico@enricozini.org>
2019-07-14 10:37:35 +0200

    Bindings for reading a stream of metadata from a file or a memory buffer. refs: #177

--- python/metadata.cc
+++ python/metadata.cc
@@ -134,6 +134,92 @@ struct to_python : public MethNoargs<to_python, arkipy_Metadata>
     }
 };
 
+std::string get_fd_name(PyObject* fd)
+{
+    pyo_unique_ptr res(throw_ifnull(PyObject_GetAttrString(fd, "name")));
+    return from_python<std::string>(res);
+}
+
+struct read : public ClassMethKwargs<read>
+{
+    constexpr static const char* name = "read";
+    constexpr static const char* signature = "src: Union[bytes, ByteIO], dest: Callable[[metadata], Optional[bool]], basedir: str=None, pathname: str=None";
+    constexpr static const char* returns = "bool";
+    constexpr static const char* summary = "Read all metadata from a give file or memory buffer";
+
+    static PyObject* run(PyTypeObject* cls, PyObject* args, PyObject* kw)
+    {
+        static const char* kwlist[] = { "src", "dest", "basedir", "pathname", NULL };
+        PyObject* py_src = nullptr;
+        PyObject* py_dest = nullptr;
+        const char* py_basedir = nullptr;
+        Py_ssize_t py_basedir_len;
+        const char* py_pathname = nullptr;
+        Py_ssize_t py_pathname_len;
+        if (!PyArg_ParseTupleAndKeywords(args, kw, "OO|z#z#",
+                    (char**)kwlist, &py_src, &py_dest,
+                    &py_basedir, &py_basedir_len, &py_pathname, &py_pathname_len))
+            return nullptr;
+
+        try {
+            metadata_dest_func dest = dest_func_from_python(py_dest);
+
+            bool res;
+            if (PyBytes_Check(py_src))
+            {
+                char* buffer;
+                Py_ssize_t length;
+                if (PyBytes_AsStringAndSize(py_src, &buffer, &length) == -1)
+                    throw PythonException();
+
+                if (py_basedir && py_pathname)
+                {
+                    arki::metadata::ReadContext ctx(
+                            std::string(py_pathname, py_pathname_len),
+                            std::string(py_basedir, py_basedir_len));
+                    res = arki::Metadata::read_buffer((uint8_t*)buffer, length, ctx, dest);
+                } else if (py_basedir) {
+                    PyErr_SetString(PyExc_ValueError, "basedir provided without pathname when parsing metadata from a memory buffer");
+                    return nullptr;
+                } else if (py_pathname) {
+                    arki::metadata::ReadContext ctx(std::string(py_pathname, py_pathname_len));
+                    res = arki::Metadata::read_buffer((uint8_t*)buffer, length, ctx, dest);
+                } else {
+                    arki::metadata::ReadContext ctx;
+                    res = arki::Metadata::read_buffer((uint8_t*)buffer, length, ctx, dest);
+                }
+            } else {
+                int fd = file_get_fileno(py_src);
+                if (fd == -1) return nullptr;
+
+                if (py_basedir && py_pathname)
+                {
+                    arki::metadata::ReadContext ctx(
+                            std::string(py_pathname, py_pathname_len),
+                            std::string(py_basedir, py_basedir_len));
+                    res = arki::Metadata::read_file(fd, ctx, dest);
+                } else if (py_basedir) {
+                    arki::metadata::ReadContext ctx(
+                            get_fd_name(py_src),
+                            std::string(py_basedir, py_basedir_len));
+                    res = arki::Metadata::read_file(fd, ctx, dest);
+                } else if (py_pathname) {
+                    arki::metadata::ReadContext ctx(std::string(py_pathname, py_pathname_len));
+                    res = arki::Metadata::read_file(fd, ctx, dest);
+                } else {
+                    arki::metadata::ReadContext ctx(get_fd_name(py_src));
+                    res = arki::Metadata::read_file(fd, ctx, dest);
+                }
+            }
+
+            if (res)
+                Py_RETURN_TRUE;
+            else
+                Py_RETURN_FALSE;
+        } ARKI_CATCH_RETURN_PYO
+    }
+};
+
 
 struct MetadataDef : public Type<MetadataDef, arkipy_Metadata>
 {
@@ -143,7 +229,7 @@ struct MetadataDef : public Type<MetadataDef, arkipy_Metadata>
 Arkimet metadata for one data item
 )";
     GetSetters<> getsetters;
-    Methods<write, make_inline, make_url, to_python> methods;
+    Methods<write, make_inline, make_url, to_python, read> methods;
 
     static void _dealloc(Impl* self)
     {
