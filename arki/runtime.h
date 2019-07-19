36d26d9c72f61ba428b0789194a97bdd03ce5806
Enrico Zini <enrico@enricozini.org>
2019-07-19 18:51:27 +0200

    Removed unused runtime::CommandLine. refs: #177

--- arki/runtime.h
+++ arki/runtime.h
@@ -35,30 +35,6 @@ struct BaseCommandLine : public utils::commandline::StandardParserWithManpage
     bool parse(int argc, const char* argv[]);
 };
 
-struct CommandLine : public BaseCommandLine
-{
-    utils::commandline::OptionGroup* inputOpts = nullptr;
-    utils::commandline::OptionGroup* outputOpts = nullptr;
-
-    utils::commandline::StringOption* stdin_input = nullptr;
-    utils::commandline::BoolOption* yaml = nullptr;
-    utils::commandline::BoolOption* json = nullptr;
-    utils::commandline::BoolOption* annotate = nullptr;
-    utils::commandline::BoolOption* summary = nullptr;
-    utils::commandline::BoolOption* summary_short = nullptr;
-    utils::commandline::StringOption* outfile = nullptr;
-    utils::commandline::StringOption* targetfile = nullptr;
-    utils::commandline::StringOption* report = nullptr;
-    utils::commandline::OptvalStringOption* archive = nullptr;
-    utils::commandline::StringOption* sort = nullptr;
-    utils::commandline::StringOption* summary_restrict = nullptr;
-
-    CommandLine(const std::string& name, int mansection=1);
-
-    /// Parse the command line
-    bool parse(int argc, const char* argv[]);
-};
-
 }
 }
 #endif
