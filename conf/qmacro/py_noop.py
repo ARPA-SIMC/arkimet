9bee9505fd18aa8a5744b4f20faafc1c3a8ac378
Enrico Zini <enrico@enricozini.org>
2019-07-29 21:50:09 +0200

    Also implement query_summary for python querymacro. refs: #177

--- conf/qmacro/py_noop.py
+++ conf/qmacro/py_noop.py
@@ -12,5 +12,5 @@ class Querymacro:
     def query_data(self, matcher=None, with_data=False, sort=None, on_metadata=None):
         return self.ds.query_data(matcher=matcher, with_data=with_data, sort=sort, on_metadata=on_metadata)
 
-    def query_summary(self, q, sum):
-        return self.ds.query_summary(q, sum)
+    def query_summary(self, matcher, summary):
+        return self.ds.query_summary(matcher, summary)
