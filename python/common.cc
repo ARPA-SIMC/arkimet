b668471af00d963922240e5d7e26e1a172a1a7cb
Enrico Zini <enrico@enricozini.org>
2019-08-01 12:22:34 +0200

    More pre-3.7 compatibility. refs: #177

--- python/common.cc
+++ python/common.cc
@@ -11,6 +11,35 @@
 
 using namespace std;
 
+extern "C" {
+
+#if Py_MAJOR_VERSION == 3 && Py_MINOR_VERSION < 7
+PyObject *ArkiPyImport_GetModule(PyObject *name)
+{
+    PyObject *m;
+    PyObject *modules = PyImport_GetModuleDict();
+    if (modules == NULL) {
+        PyErr_SetString(PyExc_RuntimeError, "unable to get sys.modules");
+        return NULL;
+    }
+    Py_INCREF(modules);
+    if (PyDict_CheckExact(modules)) {
+        m = PyDict_GetItemWithError(modules, name);  /* borrowed */
+        Py_XINCREF(m);
+    }
+    else {
+        m = PyObject_GetItem(modules, name);
+        if (m == NULL && PyErr_ExceptionMatches(PyExc_KeyError)) {
+            PyErr_Clear();
+        }
+    }
+    Py_DECREF(modules);
+    return m;
+}
+#endif
+
+}
+
 namespace arki {
 namespace python {
 
