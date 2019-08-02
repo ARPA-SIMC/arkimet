f1ce37a12a28b1b1837f2ba66e6b202dc892d56e
Enrico Zini <enrico@enricozini.org>
2019-08-02 15:05:39 +0200

    Ported more arki-server tests to python. refs: #177

--- python/tests/test_arki_server.py
+++ python/tests/test_arki_server.py
@@ -1,6 +1,7 @@
 import unittest
 import multiprocessing
 import os
+import requests
 import arkimet as arki
 import arkimet.test
 from arkimet.cmdline.server import make_server
@@ -13,11 +14,12 @@ class ServerThread(multiprocessing.Process):
         self.server_exception = None
 
     def run(self):
-        try:
-            self.server.serve_forever()
-        except Exception as e:
-            self.server_exception = e
-            self.server.shutdown()
+        with arki.test.LogCapture():
+            try:
+                self.server.serve_forever()
+            except Exception as e:
+                self.server_exception = e
+                self.server.shutdown()
 
 
 class Env(arkimet.test.Env):
@@ -34,6 +36,7 @@ class Env(arkimet.test.Env):
                 "type": "iseg",
                 "format": "grib",
                 "step": "daily",
+                "postprocess": "cat,echo,say,checkfiles,error,outthenerr",
             })
             config[name] = cfg
 
@@ -149,3 +152,125 @@ class TestArkiServer(unittest.TestCase):
 
         data = ds.query_bytes("origin:GRIB1,200", postprocess="say ciao")
         self.assertEqual(data, b"ciao\n")
+
+    def test_error(self):
+        """
+        Test the server giving an error
+        """
+        # Try it on metadata
+        res = requests.post(self.server_url + "/dataset/test200/query", data={
+            "query": "origin:GRIB1,200;MISCHIEF",
+        })
+        self.assertEqual(res.status_code, 500)
+        self.assertIn("subexpression 'MISCHIEF' does not contain a colon", res.headers["Arkimet-Exception"])
+
+        # Try it on summaries
+        res = requests.post(self.server_url + "/dataset/test200/summary", data={
+            "query": "origin:GRIB1,200;MISCHIEF",
+        })
+        self.assertEqual(res.status_code, 500)
+        self.assertIn("subexpression 'MISCHIEF' does not contain a colon", res.headers["Arkimet-Exception"])
+
+        # Try it on streams
+        res = requests.post(self.server_url + "/dataset/test200/query", data={
+            "query": "origin:GRIB1,200;MISCHIEF",
+            "style": "data",
+        })
+        self.assertEqual(res.status_code, 500)
+        self.assertIn("subexpression 'MISCHIEF' does not contain a colon", res.headers["Arkimet-Exception"])
+
+    def test_qexpand(self):
+        """
+        Test expanding a query
+        """
+        config = arki.dataset.http.load_cfg_sections(self.server_url)
+        query = arki.dataset.http.expand_remote_query(config, "origin:GRIB1,200;product:t")
+        self.assertEqual(
+                query,
+                "origin:GRIB1,200; product:GRIB1,200,2,11 or GRIB1,98,128,130 or GRIB1,98,128,167"
+                " or GRIB1,200,200,11 or GRIB2,200,0,200,11")
+
+        # Expanding a query that fails returns the query itself: if some
+        # servers expanded it consistently and all others errored out, then we
+        # would send it expanded to all of them and we are ok
+        query = arki.dataset.http.expand_remote_query(config, "origin:GRIB1,200;product:pippo")
+        self.assertEqual(query, "origin:GRIB1,200;product:pippo")
+
+        res = requests.post(self.server_url + "/qexpand", data={
+            "query": "origin:GRIB1,200;product:pippo",
+        })
+        self.assertEqual(res.status_code, 500)
+        self.assertIn("cannot parse Product style 'pippo'", res.headers["Arkimet-Exception"])
+
+    def test_qmacro_noop(self):
+        """
+        Test querying the datasets via macro
+        """
+        cfg = arki.cfg.Section(
+            name="noop",
+            type="remote",
+            path=self.server_url,
+            qmacro="test200")
+        ds = arki.dataset.Reader(cfg)
+        mdc = ds.query_data()
+        self.assertEqual(len(mdc), 1)
+        # Check that the source record that comes out is ok
+        self.assertEqual(mdc[0].to_python("source"), {
+            't': 'source', 's': 'URL', 'f': 'grib',
+            'url': self.server_url + '/query',
+        })
+
+    def test_qmacro_expa(self):
+        """
+        Test querying the datasets via macro
+        """
+        cfg = arki.cfg.Section(
+            name="expa 2007-07-08",
+            type="remote",
+            path=self.server_url,
+            qmacro="ds:test200. d:@. t:1300. s:GRIB1/0/0h/0h. l:GRIB1/1. v:GRIB1/200/140/229.",
+        )
+        ds = arki.dataset.Reader(cfg)
+        mdc = ds.query_data()
+        self.assertEqual(len(mdc), 1)
+        # Check that the source record that comes out is ok
+        self.assertEqual(mdc[0].to_python("source"), {
+            't': 'source', 's': 'URL', 'f': 'grib',
+            'url': self.server_url + '/query',
+        })
+
+    def test_qmacro_summary(self):
+        """
+        Test querying the summary
+        """
+        cfg = arki.cfg.Section(
+            name="noop",
+            type="remote",
+            path=self.server_url,
+            qmacro="test200")
+        ds = arki.dataset.Reader(cfg)
+        summary = ds.query_summary()
+        self.assertEqual(summary.count, 1)
+
+    def test_postproc_error(self):
+        """
+        Test a postprocessor that outputs data and then exits with error
+        """
+        config = arki.dataset.http.load_cfg_sections(self.server_url)
+        ds = arki.dataset.Reader(config["test200"])
+
+        # Querying it should get an error
+        with self.assertRaises(RuntimeError) as e:
+            ds.query_bytes(postprocess="error")
+        self.assertIn("FAIL", str(e.exception))
+
+    def test_postproc_outthenerr(self):
+        """
+        Test a postprocessor that outputs data and then exits with error
+        """
+        config = arki.dataset.http.load_cfg_sections(self.server_url)
+        ds = arki.dataset.Reader(config["test200"])
+
+        # Querying it should get the partial output and no error
+        data = ds.query_bytes(postprocess="outthenerr")
+        self.assertEqual(data, b"So far, so good\n")
