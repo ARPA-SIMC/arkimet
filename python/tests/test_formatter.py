d690ae06ed173d4360046001503bb7303e38eca6
Enrico Zini <enrico@enricozini.org>
2019-08-08 16:08:02 +0200

    Test vm2 area formatting. refs: #177

--- python/tests/test_formatter.py
+++ python/tests/test_formatter.py
@@ -1,5 +1,6 @@
 import unittest
 import arkimet as arki
+from arkimet.test import skip_unless_vm2
 
 
 class TestFormatter(unittest.TestCase):
@@ -7,3 +8,17 @@ class TestFormatter(unittest.TestCase):
         formatter = arki.Formatter()
         res = formatter.format({"type": "origin", "style": "GRIB1", "centre": 200, "subcentre": 0, "process": 0})
         self.assertEqual(res, "GRIB1 from ARPAE SIMC Emilia Romagna, subcentre 0, process 0")
+
+    def test_vm2(self):
+        skip_unless_vm2()
+
+        ds = arki.dataset.Reader({
+            "format": "vm2",
+            "name": "fixture.vm2",
+            "path": "inbound/fixture.vm2",
+            "type": "file",
+        })
+        mds = ds.query_data()
+        formatter = arki.Formatter()
+        res = formatter.format(mds[0].to_python("area"))
+        self.assertEqual(res, "id=1, lat=4460016, lon=1207738, rep=locali")
