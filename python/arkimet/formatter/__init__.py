87ccff22d65f3822da1540a7531ec2220f6a563b
Enrico Zini <enrico@enricozini.org>
2019-08-08 08:45:55 +0200

    Moved default formatters from /etc to arkimet.formatter module. refs: #177

--- python/arkimet/formatter/__init__.py
+++ python/arkimet/formatter/__init__.py
@@ -1,3 +1,10 @@
 from .formatter import Formatter
+from . import origin
+from . import product
+from . import level
+from . import timerange
+from . import area
+from . import proddef
+
 
 __all__ = [Formatter]
