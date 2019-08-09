63321a2ad274c5035dd1c06a07351e7894d03526
Enrico Zini <enrico@enricozini.org>
2019-08-09 17:14:18 +0200

    Started porting grib scanners from lua to python. refs: #177

---
+++ conf/scan-grib/grib2.py
@@ -0,0 +1,140 @@
+from arkimet.scan.grib import Scanner
+
+
+# 	function norm_lon(lon)
+# 		if lon > 180 then
+# 			return lon - 360
+# 		else
+# 			return lon
+# 		end
+# 	end
+
+def scan_grib2(grib, md):
+    # Reference time
+#	local reftime = arki_time.time(grib.year, grib.month, grib.day, grib.hour, grib.minute, grib.second)
+#	md:set(arki_reftime.position(reftime))
+
+    # Run
+#	md:set(arki_run.minute(grib.hour, grib.minute))
+
+    # Origin
+    md["origin"] = {
+        "style": "GRIB2",
+        "centre": grib.get_long("centre"),
+        "subcentre": grib.get_long("subCentre"),
+        "process_type": grib.get_long("typeOfGeneratingProcess"),
+        "background_process_id": grib.get_long("backgroundGeneratingProcessIdentifier"),
+        "process_id": grib.get_long("generatingProcessIdentifier"),
+    }
+
+    # Product
+    md["product"] = {
+        "style": "GRIB2",
+        "centre": grib.get_long("centre"),
+        "discipline": grib.get_long("discipline"),
+        "category": grib.get_long("parameterCategory"),
+        "number": grib.get_long("parameterNumber"),
+        "table_version": grib.get_long("tablesVersion"),
+        "local_table_version": grib.get_long("localTablesVersion"),
+    }
+
+    # Level
+#	local ltype1, lscale1, lvalue1 = gribl.typeOfFirstFixedSurface, 0, 0
+#	if gribl.scaleFactorOfFirstFixedSurface ~= -1 then
+#		lscale1 = gribl.scaleFactorOfFirstFixedSurface
+#		lvalue1 = gribl.scaledValueOfFirstFixedSurface
+#	end
+#
+#	if grib.typeOfSecondFixedSurface and grib.typeOfSecondFixedSurface ~= 255 then
+#		local ltype2, lscale2, lvalue2 = grib.typeOfSecondFixedSurface, 0, 0
+#		if gribl.scaleFactorOfSecondFixedSurface ~= -1 then
+#			lscale2 = grib.scaleFactorOfSecondFixedSurface
+#			lvalue2 = grib.scaledValueOfSecondFixedSurface
+#		end
+#		md:set(arki_level.grib2d(ltype1, lscale1, lvalue1, ltype2, lscale2, lvalue2))
+#	else
+#		md:set(arki_level.grib2s(ltype1, lscale1, lvalue1))
+#	end
+
+    # Time range
+#    local tr_ft = gribl.forecastTime
+#    local tr_ftu = gribl.indicatorOfUnitOfTimeRange
+#    if tr_ft ~= nil and tr_ftu ~= nil then
+#        local pdtn = gribl.productDefinitionTemplateNumber
+#        if pdtn >= 0 and pdtn <= 7 then
+#            md:set(arki_timerange.timedef(tr_ft, tr_ftu, 254, 0, "s"))
+#        else
+#            local tr_sp = gribl.typeOfStatisticalProcessing
+#            local tr_spu = gribl.indicatorOfUnitForTimeRange
+#            local tr_spl = gribl.lengthOfTimeRange
+#            if tr_sp ~= nil and tr_spu ~= nil and tr_spl ~= nil then
+#                if pdtn >= 8 and pdtn <= 14 and gribl.typeOfProcessedData == 0 then
+#                    md:set(arki_timerange.timedef(0, "s", tr_sp, tr_spl, tr_spu))
+#                else
+#                    md:set(arki_timerange.timedef_combined(tr_ft, tr_ftu, tr_sp, tr_spl, tr_spu))
+#                end
+#            else
+#                if pdtn >= 8 and pdtn <= 14 and gribl.typeOfProcessedData == 0 then
+#                    md:set(arki_timerange.timedef(0, "s"))
+#                else
+#                    md:set(arki_timerange.timedef(tr_ft, tr_ftu))
+#                end
+#            end
+#        end
+#    end
+
+    # Area
+#	local area = {}
+#	area.tn = grib.gridDefinitionTemplateNumber
+#    if area.tn == 32768 then
+#        area.utm = 1
+#        area.latfirst = grib.northingOfFirstGridPoint
+#        area.lonfirst = grib.eastingOfFirstGridPoint
+#        area.latlast = grib.northingOfLastGridPoint
+#        area.lonlast = grib.eastingOfLastGridPoint
+#        area.fe = grib.falseEasting
+#        area.fn = grib.falseNorthing
+#        area.zone = grib.zone
+#	area.Ni = grib.Ni
+#	area.Nj = grib.Nj
+#    else
+#      if grib.latitudeOfFirstGridPointInDegrees ~= nil then
+#        area.latfirst = grib.latitudeOfFirstGridPointInDegrees * 1000000
+#        area.lonfirst = grib.longitudeOfFirstGridPointInDegrees * 1000000
+#
+#        if grib.numberOfPointsAlongAParallel then
+#            area.Ni = grib.numberOfPointsAlongAParallel
+#            area.Nj = grib.numberOfPointsAlongAMeridian
+#            area.latlast = grib.latitudeOfLastGridPointInDegrees * 1000000
+#            area.lonlast = grib.longitudeOfLastGridPointInDegrees * 1000000
+#        end
+#        if grib.numberOfPointsAlongXAxis then
+#            area.Ni = grib.numberOfPointsAlongXAxis
+#            area.Nj = grib.numberOfPointsAlongYAxis
+#        end
+#        if grib.angleOfRotationInDegrees then
+#            area.rot = grib.angleOfRotationInDegrees * 1000000
+#            area.latp = grib.latitudeOfSouthernPoleInDegrees * 1000000
+#            area.lonp = grib.longitudeOfSouthernPoleInDegrees * 1000000
+#        end
+#      end
+#    end
+#	md:set(arki_area.grib(area))
+
+    # Proddef
+#    local proddef = {}
+#    proddef.tod = gribl.typeOfProcessedData
+#	if gribl.typeOfProcessedData >= 3 and gribl.typeOfProcessedData <= 5 then
+#		proddef.mc = grib.marsClass
+#		proddef.mt = gribl.marsType
+#		proddef.ty = grib.typeOfEnsembleForecast
+#		proddef.pf = grib.perturbationNumber
+#		proddef.tf = grib.numberOfForecastsInEnsemble
+#		if grib.derivedForecast then
+#			proddef.d = grib.derivedForecast
+#		end
+#	end
+#    md:set(arki_proddef.grib(proddef))
+
+
+Scanner.register(2, scan_grib2)
--- python/scan.cc
+++ python/scan.cc
@@ -1,34 +1,17 @@
 #include "scan.h"
 #include "common.h"
 #include "structured.h"
+#include "metadata.h"
 #include "utils/methods.h"
+#include "utils/type.h"
+#include "arki/metadata.h"
 #include "arki/values.h"
 #include "arki/utils/vm2.h"
 #include "arki/utils/sys.h"
 #include "arki/utils/string.h"
 #include "arki/runtime.h"
 #include "arki/scan/grib.h"
-#if 0
-#include "metadata.h"
-#include "summary.h"
-#include "cfg.h"
-#include "files.h"
-#include "matcher.h"
-#include "utils/values.h"
-#include "utils/type.h"
-#include "utils/dict.h"
-#include "arki/core/file.h"
-#include "arki/core/cfg.h"
-#include "arki/dataset.h"
-#include "arki/dataset/http.h"
-#include "arki/dataset/time.h"
-#include "arki/dataset/segmented.h"
-#include "arki/nag.h"
-#include "dataset/reporter.h"
-#include "arki/sort.h"
-#include <vector>
-#include "config.h"
-#endif
+#include <grib_api.h>
 
 using namespace std;
 using namespace arki;
@@ -36,13 +19,36 @@ using namespace arki::core;
 using namespace arki::utils;
 using namespace arki::python;
 
+extern "C" {
+
+PyTypeObject* arkipy_scan_Grib_Type = nullptr;
+
+}
+
 namespace {
+
+inline void check_grib_error(int res, const char* msg)
+{
+    if (res)
+    {
+        PyErr_Format(PyExc_KeyError, "%s: %s", msg, grib_get_error_message(res));
+        throw PythonException();
+    }
+}
+
+arkipy_scan_Grib* grib_create(grib_handle* gh)
+{
+    arkipy_scan_Grib* result = PyObject_New(arkipy_scan_Grib, arkipy_scan_Grib_Type);
+    if (!result) throw PythonException();
+    result->gh = gh;
+    return result;
+}
+
 
 /*
  * scan.grib module functions
  */
 
-#if 0
 PyObject* gribscanner_object = nullptr;
 
 void load_gribscanner_object()
@@ -74,7 +80,7 @@ void load_gribscanner_object()
 
     // Get arkimet.scan.grib.GribScanner
     pyo_unique_ptr module(throw_ifnull(PyImport_ImportModule("arkimet.scan.grib")));
-    pyo_unique_ptr cls(throw_ifnull(PyObject_GetAttrString(module, "GribScanner")));
+    pyo_unique_ptr cls(throw_ifnull(PyObject_GetAttrString(module, "Scanner")));
     pyo_unique_ptr obj(throw_ifnull(PyObject_CallFunction(cls, nullptr)));
 
     // Hold a reference to arki.python.BBox forever once loaded the first time
@@ -82,17 +88,19 @@ void load_gribscanner_object()
 }
 
 
-class PythonGrbScanner : public arki::scan::GribScanner
+class PythonGribScanner : public arki::scan::GribScanner
 {
 protected:
-    void scan(grib_handle* gh, Metadata& md) override
+    void scan(grib_handle* gh, std::shared_ptr<Metadata> md) override
     {
         AcquireGIL gil;
         if (!gribscanner_object)
             load_gribscanner_object();
 
+        pyo_unique_ptr pygh((PyObject*)grib_create(gh));
+        pyo_unique_ptr pymd((PyObject*)metadata_create(md));
         pyo_unique_ptr obj(PyObject_CallMethod(
-                        gribscanner_object, "scan", "OO", Py_None, e.res.get()));
+                        gribscanner_object, "scan", "OO", pygh.get(), pymd.get()));
     }
 
 public:
@@ -102,63 +110,232 @@ public:
     virtual ~PythonGribScanner()
     {
     }
+};
 
-    std::unique_ptr<arki::utils::geos::Geometry> compute(const arki::types::Area& v) const override
+
+struct edition : public Getter<edition, arkipy_scan_Grib>
+{
+    constexpr static const char* name = "edition";
+    constexpr static const char* doc = "return the GRIB edition";
+    constexpr static void* closure = nullptr;
+
+    static PyObject* get(Impl* self, void* closure)
     {
-
-        arki::python::PythonEmitter e;
-        v.serialise(e, arki::structured::keys_python);
-
-
-        if (!obj)
-        {
-            PyObject *type, *value, *traceback;
-            PyErr_Fetch(&type, &value, &traceback);
-            pyo_unique_ptr exc_type(type);
-            pyo_unique_ptr exc_value(value);
-            pyo_unique_ptr exc_traceback(traceback);
-
-            arki::nag::debug_tty("python bbox failed: %s", exc_type.str().c_str());
-            arki::nag::warning("python bbox failed: %s", exc_type.str().c_str());
-            return std::unique_ptr<arki::utils::geos::Geometry>();
-        }
-
-        if (obj == Py_None)
-            return std::unique_ptr<arki::utils::geos::Geometry>();
-
-        Py_ssize_t size = PyList_Size(obj);
-        if (size == -1)
-            throw PythonException();
-
-        switch (size)
-        {
-            case 0: return std::unique_ptr<arki::utils::geos::Geometry>();
-            case 1:
-            {
-                auto gf = arki::utils::geos::GeometryFactory::getDefaultInstance();
-                PyObject* pair = PyList_GET_ITEM(obj.get(), 0); // Borrowed reference
-                auto point = get_coord_pair(pair);
-                return std::unique_ptr<arki::utils::geos::Geometry>(
-                        gf->createPoint(arki::utils::geos::Coordinate(point.second, point.first)));
-            }
-            default:
-            {
-                auto gf = arki::utils::geos::GeometryFactory::getDefaultInstance();
-                arki::utils::geos::CoordinateArraySequence cas;
-                for (Py_ssize_t i = 0; i < size; ++i)
-                {
-                    PyObject* pair = PyList_GET_ITEM(obj.get(), i); // Borrowed reference
-                    auto point = get_coord_pair(pair);
-                    cas.add(arki::utils::geos::Coordinate(point.second, point.first));
-                }
-                std::unique_ptr<arki::utils::geos::LinearRing> lr(gf->createLinearRing(cas));
-                return std::unique_ptr<arki::utils::geos::Geometry>(
-                        gf->createPolygon(*lr, std::vector<arki::utils::geos::Geometry*>()));
-            }
-        }
+        try {
+            long edition;
+            check_grib_error(grib_get_long(self->gh, "editionNumber", &edition), "cannot read edition number");
+            return to_python((int)edition);
+        } ARKI_CATCH_RETURN_PYO;
     }
 };
+
+struct get_long : public MethKwargs<get_long, arkipy_scan_Grib>
+{
+    constexpr static const char* name = "get_long";
+    constexpr static const char* signature = "str";
+    constexpr static const char* returns = "int";
+    constexpr static const char* summary = "return the long value of a grib key";
+    constexpr static const char* doc = nullptr;
+
+    static PyObject* run(Impl* self, PyObject* args, PyObject* kw)
+    {
+        static const char* kwlist[] = { "key", NULL };
+        const char* key = nullptr;
+        if (!PyArg_ParseTupleAndKeywords(args, kw, "s", (char**)kwlist, &key))
+            return nullptr;
+
+        try {
+            // Push the function result for lua
+            long val;
+            int res = grib_get_long(self->gh, key, &val);
+            if (res == GRIB_NOT_FOUND)
+                Py_RETURN_NONE;
+
+            check_grib_error(res, "cannot read long value from grib");
+
+            return to_python(val);
+        } ARKI_CATCH_RETURN_PYO
+    }
+};
+
+#if 0
+
+// Lookup a grib value for grib.<fieldname>
+int GribLua::arkilua_lookup_gribs(lua_State* L)
+{
+    GribLua& s = get_griblua(L, 1, "gribs");
+    grib_handle* gh = s.gh;
+
+	// Get the name to lookup from lua
+	// (we use 2 because 1 is the table, since we are a __index function)
+	luaL_checkstring(L, 2);
+	const char* name = lua_tostring(L, 2);
+
+	// Push the function result for lua
+	const int maxsize = 1000;
+	char buf[maxsize];
+	size_t len = maxsize;
+	int res = grib_get_string(gh, name, buf, &len);
+	if (res == GRIB_NOT_FOUND)
+	{
+		lua_pushnil(L);
+	} else {
+		arkilua_check_gribapi(L, res, "reading string value");
+		if (len > 0) --len;
+		lua_pushlstring(L, buf, len);
+	}
+
+	// Number of values we're returning to lua
+	return 1;
+}
+
+// Lookup a grib value for grib.<fieldname>
+int GribLua::arkilua_lookup_gribd(lua_State* L)
+{
+    GribLua& s = get_griblua(L, 1, "gribd");
+    grib_handle* gh = s.gh;
+
+	// Get the name to lookup from lua
+	// (we use 2 because 1 is the table, since we are a __index function)
+	luaL_checkstring(L, 2);
+	const char* name = lua_tostring(L, 2);
+
+	// Push the function result for lua
+	double val;
+	int res = grib_get_double(gh, name, &val);
+	if (res == GRIB_NOT_FOUND)
+	{
+		lua_pushnil(L);
+	} else {
+		arkilua_check_gribapi(L, res, "reading double value");
+		lua_pushnumber(L, val);
+	}
+
+	// Number of values we're returning to lua
+	return 1;
+}
 #endif
+
+struct GribDef : public Type<GribDef, arkipy_scan_Grib>
+{
+    constexpr static const char* name = "Grib";
+    constexpr static const char* qual_name = "arkimet.scan.grib.Grib";
+    constexpr static const char* doc = R"(
+Access grib message contents
+)";
+    GetSetters<edition> getsetters;
+    Methods<get_long> methods;
+
+    static void _dealloc(Impl* self)
+    {
+        Py_TYPE(self)->tp_free(self);
+    }
+
+    static PyObject* _str(Impl* self)
+    {
+        return PyUnicode_FromString("Grib");
+    }
+
+    static PyObject* _repr(Impl* self)
+    {
+        return PyUnicode_FromString("Grib");
+    }
+
+    static int _init(Impl* self, PyObject* args, PyObject* kw)
+    {
+        // Grib() should not be invoked as a constructor
+        PyErr_SetString(PyExc_NotImplementedError, "Grib objects cannot be constructed explicitly");
+        return -1;
+    }
+
+#if 0
+    static int sq_contains(Impl* self, PyObject* py_key)
+    {
+        try {
+            std::string key = from_python<std::string>(py_key);
+            types::Code code = types::parseCodeName(key);
+            if (code == arki::TYPE_SOURCE)
+                return self->md->has_source() ? 1 : 0;
+            else
+                return self->md->has(code) ? 1 : 0;
+        } ARKI_CATCH_RETURN_INT
+    }
+
+    static PyObject* mp_subscript(Impl* self, PyObject* py_key)
+    {
+        try {
+            std::string key = from_python<std::string>(py_key);
+            types::Code code = types::parseCodeName(key);
+            if (code == TYPE_SOURCE)
+            {
+                if (!self->md->has_source())
+                    return PyErr_Format(PyExc_KeyError, "section not found: '%s'", key.c_str());
+                return python::to_python(self->md->source().to_string());
+            } else {
+                const types::Type* res = self->md->get(code);
+                if (!res)
+                    return PyErr_Format(PyExc_KeyError, "section not found: '%s'", key.c_str());
+                return python::to_python(res->to_string());
+            }
+        } ARKI_CATCH_RETURN_PYO
+    }
+// Lookup a grib value for grib.<fieldname>
+int GribLua::arkilua_lookup_grib(lua_State* L)
+{
+    // Fetch the Scanner reference from the userdata value
+    GribLua& s = get_griblua(L, 1, "grib");
+    grib_handle* gh = s.gh;
+
+	// Get the name to lookup from lua
+	// (we use 2 because 1 is the table, since we are a __index function)
+	luaL_checkstring(L, 2);
+	const char* name = lua_tostring(L, 2);
+
+	// Get information about the type
+	int type;
+	int res = grib_get_native_type(gh, name, &type);
+	if (res == GRIB_NOT_FOUND)
+		type = GRIB_TYPE_MISSING;
+	else
+		arkilua_check_gribapi(L, res, "getting type of key %s", name);
+
+	// Look up the value and push the function result for lua
+	switch (type)
+	{
+		case GRIB_TYPE_LONG: {
+			long val;
+			arkilua_check_gribapi(L, grib_get_long(gh, name, &val), "reading long value %s", name);
+			lua_pushnumber(L, val);
+			break;
+		}
+		case GRIB_TYPE_DOUBLE: {
+			double val;
+			arkilua_check_gribapi(L, grib_get_double(gh, name, &val), "reading double value %s", name);
+			lua_pushnumber(L, val);
+			break;
+		} 
+		case GRIB_TYPE_STRING: {
+			const int maxsize = 1000;
+			char buf[maxsize];
+			size_t len = maxsize;
+			arkilua_check_gribapi(L, grib_get_string(gh, name, buf, &len), "reading string value %s", name);
+			if (len > 0) --len;
+			lua_pushlstring(L, buf, len);
+			break;
+		} 
+		default:
+		    lua_pushnil(L);
+			break;
+	}
+
+	// Number of values we're returning to lua
+	return 1;
+}
+#endif
+};
+
+GribDef* grib_def = nullptr;
+
 
 /*
  * scan.vm2 module functions
@@ -211,6 +388,7 @@ struct vm2_get_variable : public MethKwargs<vm2_get_variable, PyObject>
 };
 
 Methods<> scan_methods;
+Methods<> grib_methods;
 Methods<vm2_get_station, vm2_get_variable> vm2_methods;
 
 }
@@ -223,6 +401,18 @@ static PyModuleDef scan_module = {
     "Arkimet format-specific functions",  /* m_doc */
     -1,             /* m_size */
     scan_methods.as_py(),    /* m_methods */
+    nullptr,           /* m_slots */
+    nullptr,           /* m_traverse */
+    nullptr,           /* m_clear */
+    nullptr,           /* m_free */
+};
+
+static PyModuleDef grib_module = {
+    PyModuleDef_HEAD_INIT,
+    "grib",         /* m_name */
+    "Arkimet GRIB-specific functions",  /* m_doc */
+    -1,             /* m_size */
+    grib_methods.as_py(),    /* m_methods */
     nullptr,           /* m_slots */
     nullptr,           /* m_traverse */
     nullptr,           /* m_clear */
@@ -248,14 +438,30 @@ namespace python {
 
 void register_scan(PyObject* m)
 {
+    pyo_unique_ptr grib = throw_ifnull(PyModule_Create(&grib_module));
+    grib_def = new GribDef;
+    grib_def->define(arkipy_scan_Grib_Type, grib);
+
     pyo_unique_ptr vm2 = throw_ifnull(PyModule_Create(&vm2_module));
     pyo_unique_ptr scan = throw_ifnull(PyModule_Create(&scan_module));
+
+    if (PyModule_AddObject(scan, "grib", grib.release()) == -1)
+        throw PythonException();
 
     if (PyModule_AddObject(scan, "vm2", vm2.release()) == -1)
         throw PythonException();
 
     if (PyModule_AddObject(m, "scan", scan.release()) == -1)
         throw PythonException();
+}
+
+namespace scan {
+void init()
+{
+    arki::scan::Scanner::register_factory("grib", [] {
+        return std::unique_ptr<arki::scan::Scanner>(new PythonGribScanner);
+    });
+}
 }
 
 }
