7f1dd7dba08965f86c9fd6e219ed65097a5a59f3
Enrico Zini <enrico@enricozini.org>
2019-08-24 13:54:41 +0200

    Ported one test case away from legacy tests. refs: #177

--- arki/matcher/utils-test.cc
+++ arki/matcher/utils-test.cc
@@ -1,4 +1,3 @@
-#include "arki/tests/legacy.h"
 #include "arki/tests/tests.h"
 #include "arki/matcher/utils.h"
 
@@ -18,43 +17,43 @@ class Tests : public TestCase
 void Tests::register_tests() {
 
 add_method("optionalcommalist", [] {
-	OptionalCommaList l("CIAO,,1,2,,3");
-	ensure(l.has(0));
-	ensure(!l.has(1));
-	ensure(l.has(2));
-	ensure(l.has(3));
-	ensure(!l.has(4));
-	ensure(l.has(5));
-	ensure(!l.has(6));
-	ensure(!l.has(100));
+    OptionalCommaList l("CIAO,,1,2,,3");
+    wassert_true(l.has(0));
+    wassert_false(l.has(1));
+    wassert_true(l.has(2));
+    wassert_true(l.has(3));
+    wassert_false(l.has(4));
+    wassert_true(l.has(5));
+    wassert_false(l.has(6));
+    wassert_false(l.has(100));
 
-	ensure_equals(l.getInt(1, 100), 100);
-	ensure_equals(l.getInt(2, 100), 1);
-	ensure_equals(l.getInt(3, 100), 2);
-	ensure_equals(l.getInt(4, 100), 100);
-	ensure_equals(l.getInt(5, 100), 3);
-	ensure_equals(l.getInt(6, 100), 100);
-	ensure_equals(l.getInt(100, 100), 100);
+    wassert(actual(l.getInt(1, 100)) == 100);
+    wassert(actual(l.getInt(2, 100)) == 1);
+    wassert(actual(l.getInt(3, 100)) == 2);
+    wassert(actual(l.getInt(4, 100)) == 100);
+    wassert(actual(l.getInt(5, 100)) == 3);
+    wassert(actual(l.getInt(6, 100)) == 100);
+    wassert(actual(l.getInt(100, 100)) == 100);
 
-    ensure_equals(l.getDouble(1,100),   100.0);
-    ensure_equals(l.getDouble(2,100),   1.0);
-    ensure_equals(l.getDouble(3,100),   2.0);
-    ensure_equals(l.getDouble(4,100),   100.0);
-    ensure_equals(l.getDouble(5,100),   3.0);
-    ensure_equals(l.getDouble(6,100),   100.0);
-    ensure_equals(l.getDouble(100,100),     100.0);
+    wassert(actual(l.getDouble(1,100)) ==   100.0);
+    wassert(actual(l.getDouble(2,100)) ==   1.0);
+    wassert(actual(l.getDouble(3,100)) ==   2.0);
+    wassert(actual(l.getDouble(4,100)) ==   100.0);
+    wassert(actual(l.getDouble(5,100)) ==   3.0);
+    wassert(actual(l.getDouble(6,100)) ==   100.0);
+    wassert(actual(l.getDouble(100,100)) == 100.0);
 });
 
 add_method("commajoiner", [] {
-	CommaJoiner j;
-	j.add("ciao");
-	j.addUndef();
-	j.add(3);
-	j.add(3.14);
-	j.addUndef();
-	j.addUndef();
+    CommaJoiner j;
+    j.add("ciao");
+    j.addUndef();
+    j.add(3);
+    j.add(3.14);
+    j.addUndef();
+    j.addUndef();
 
-	ensure_equals(j.join(), "ciao,,3,3.14");
+    wassert(actual(j.join()) == "ciao,,3,3.14");
 });
 
 }
