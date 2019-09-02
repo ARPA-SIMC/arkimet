b4c31b1c8ea8726c7a50f3ad9d723c7226732666
Enrico Zini <enrico@enricozini.org>
2019-09-02 15:36:20 +0200

    Added a mock bounding box calculator for tests. refs: #177

--- arki/bbox.cc
+++ arki/bbox.cc
@@ -20,6 +20,54 @@ struct NullBBox : public BBox
     }
 };
 
+struct MockBBox : public BBox
+{
+    mutable std::map<std::string, std::string> db;
+    mutable geos::io::WKTReader* reader = nullptr;
+
+    MockBBox()
+    {
+    }
+
+    ~MockBBox()
+    {
+        delete reader;
+    }
+
+    void init() const
+    {
+        reader = new geos::io::WKTReader;
+        // Fill db
+        db["GRIB(Ni=441, Nj=181, latfirst=45000000, latlast=43000000, lonfirst=10000000, lonlast=12000000, type=0)"] =
+            "POLYGON ((10 45, 10 43, 12 43, 12 45, 10 45))",
+        db["GRIB(Ni=441, Nj=181, latfirst=75000000, latlast=30000000, lonfirst=-45000000, lonlast=65000000, type=0)"] =
+            "POLYGON ((-45 75, -45 30, 65 30, 65 75, -45 75))";
+        db["GRIB(blo=0, lat=4502770, lon=966670, sta=101)"] =
+            "POINT (9.666700000000001 45.0277)";
+        db["ODIMH5(lat=44456700, lon=11623600, radius=100000)"] =
+            "POLYGON ((11.9960521084854 45.35574923752996, 12.52266823017452 44.82910625443706, 12.52266823017452 44.08429374556294, 11.9960521084854 43.55765076247004, 11.2511478915146 43.55765076247004, 10.72453176982548 44.08429374556294, 10.72453176982548 44.82910625443706, 11.2511478915146 45.35574923752996, 11.9960521084854 45.35574923752996))";
+    }
+
+    std::unique_ptr<arki::utils::geos::Geometry> compute(const types::Area& v) const override
+    {
+        // Lazily load area values used in tests
+        if (db.empty())
+            init();
+
+        std::string key = v.to_string();
+        auto val = db.find(key);
+        if (val == db.end())
+        {
+            fprintf(stderr, "MOCK MISSING %s\n", key.c_str());
+            return std::unique_ptr<arki::utils::geos::Geometry>();
+        }
+        else
+        {
+            return std::unique_ptr<arki::utils::geos::Geometry>(reader->read(val->second));
+        }
+    }
+};
+
 }
 
 static std::function<std::unique_ptr<BBox>()> factory;
@@ -33,7 +81,7 @@ unique_ptr<BBox> BBox::create()
 {
     if (factory)
         return factory();
-    return unique_ptr<BBox>(new NullBBox);
+    return unique_ptr<BBox>(new MockBBox);
 }
 
 void BBox::set_factory(std::function<std::unique_ptr<BBox>()> new_factory)
