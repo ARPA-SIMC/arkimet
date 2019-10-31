073bbfef609d95f28590eef17c52a9b47b9bdef7
Emanuele Di Giacomo <edigiacomo@arpae.it>
2019-10-31 15:33:49 +0100

    Scan timerange for generic #191

--- conf/scan/bufr_generic.py
+++ conf/scan/bufr_generic.py
@@ -1,11 +1,63 @@
-# This does not currently seem to be invoked
-#
-# function scan(msg, md)
-#     local area = bufr_read_area_fixed(msg)
-#     local proddef = bufr_read_proddef(msg)
-#     local report = msg:find("rep_memo")
-#     if area then md:set(arki_area.grib(area)) end
-#     if proddef then md:set(arki_proddef.grib(proddef)) end
-#     if report then md:set(md.product:addValues{t=report:enqc()}) end
-#     md:set(bufr_scan_forecast(msg))
-# end
+from arkimet.scan.bufr import Scanner, read_area_fixed, read_proddef
+from arkimet.scan import timedef
+
+
+def read_timerange(msg):
+    # Set timerange
+    p1_list = {d["trange"].p1 for d in msg.query_data()}
+    if len(p1_list) > 1:
+        raise Exception("BUFR message has contradictory forecasts {}".format(
+            p1_list
+        ))
+
+    p1 = p1_list.pop() if p1_list else 0
+    unit = timedef.UNIT_SECOND
+
+    for d, u in (
+        (3600, timedef.UNIT_HOUR),
+        (60, timedef.UNIT_MINUTE),
+    ):
+        if p1 % d == 0:
+            p1 = p1 // d
+            unit = u
+            break
+
+    return {
+        "style": "Timedef",
+        "step_len": p1,
+        "step_unit": unit,
+    }
+
+def is_generic(md):
+    product = md.to_python("product")
+    return product.get("type") == 255 and product.get("subtype") == 255
+
+
+def scan(msg, md):
+    # Set area and proddef
+    area = read_area_fixed(msg)
+    proddef = read_proddef(msg)
+
+    if area:
+        md["area"] = {"style": "GRIB", "value": area}
+
+    if proddef:
+        md["proddef"] = {"style": "GRIB", "value": proddef}
+
+    # Set product based on report name
+    if msg.report:
+        product = md.to_python("product")
+        product["value"]["t"] = msg.report
+        md["product"] = product
+
+    md["timerange"] = read_timerange(msg)
+
+
+def scan_temp(msg, md):
+    # Add timerange to generic with rep_memo=temp
+    if is_generic(md):
+        md["timerange"] = read_timerange(msg)
+
+
+Scanner.register("generic", scan)
+Scanner.register("temp", scan_temp, 1)
