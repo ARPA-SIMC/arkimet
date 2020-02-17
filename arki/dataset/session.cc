6656a75d4b554b7ba950caecd1f1db9bc8bc46d4
Enrico Zini <enrico@enricozini.org>
2020-02-17 10:16:34 +0100

    Prevent unneeded deletion/recreation of hash entries. refs: #213

--- arki/dataset/session.cc
+++ arki/dataset/session.cc
@@ -20,11 +20,6 @@ std::shared_ptr<segment::Reader> Session::reader_from_pool(const std::string& ab
     auto res = reader_pool.find(abspath);
     if (res == reader_pool.end())
         return std::shared_ptr<segment::Reader>();
-    if (res->second.expired())
-    {
-        reader_pool.erase(res);
-        return std::shared_ptr<segment::Reader>();
-    }
     return res->second.lock();
 }
 
