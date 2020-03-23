ded8ad6761b3f57bb8d7c19160852f512f5de877
Enrico Zini <enrico@enricozini.org>
2020-03-23 14:13:52 +0100

    Test 500 server errors during normal query. refs: #221

--- arki/dataset/http-test.cc
+++ arki/dataset/http-test.cc
@@ -1,5 +1,7 @@
 #include "arki/tests/tests.h"
 #include "arki/tests/daemon.h"
+#include "arki/dataset/session.h"
+#include "arki/dataset/query.h"
 #include "http.h"
 
 namespace {
@@ -81,6 +83,24 @@ add_method("redirect", [] {
     auto sec = cfg.section("test200");
     wassert(actual(sec->value("type")) == "remote");
     wassert(actual(sec->value("path")) == "http://foo.bar/foo/dataset/test200");
+});
+
+add_method("server_error", [] {
+    tests::Daemon server({"arki/dataset/http-test-daemon", "--action=query500"});
+    int port;
+    sscanf(server.daemon_start_string.c_str(), "OK %d\n", &port);
+    char url[512];
+    snprintf(url, 512, "http://localhost:%d", port);
+
+    auto cfg = http::Reader::load_cfg_sections(url);
+    auto session = std::make_shared<dataset::Session>();
+    auto sec = cfg.section("test200");
+    wassert(actual(sec->value("type")) == "remote");
+    auto ds = std::make_shared<http::Dataset>(session, *sec);
+    auto reader = ds->create_reader();
+    unsigned count = 0;
+    wassert_throws(std::runtime_error, reader->query_data(dataset::DataQuery(""), [&](std::shared_ptr<Metadata>) { ++count; return true; }));
+    wassert(actual(count) == 0u);
 });
 
 }
