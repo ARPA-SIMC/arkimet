5f9e87c04716f18cf3b488aee49ef8be58358d95
Enrico Zini <enrico@enricozini.org>
2020-07-07 11:56:07 +0200

    Tried (and failed) to reproduce segfault in #230. refs: #230

--- python/tests/test_summary.py
+++ python/tests/test_summary.py
@@ -3,6 +3,8 @@ from typing import Union
 import unittest
 import io
 import os
+import tempfile
+import json
 import arkimet as arki
 
 
@@ -130,3 +132,59 @@ class TestSummary(unittest.TestCase):
             s.write_short(out, format="json", annotate=True)
             self.assertEqual(out.getvalue()[:26], b'{"items":{"summarystats":{')
             self.assertIn(b'"desc":"', out.getvalue())
+
+    def test_issue230(self):
+        self.maxDiff = None
+        # Read the binary summary
+        with open("inbound/issue230.summary", "rb") as fd:
+            summary = arki.Summary.read_binary(fd)
+
+        # Serialize to JSON to check that what we read matches the sample
+        with tempfile.NamedTemporaryFile("w+b") as out:
+            summary.write(out, format="json")
+            out.seek(0)
+            parsed = json.load(out)
+
+        self.assertCountEqual(parsed["items"], [
+            {'origin': {'s': 'GRIB1', 'ce': 80, 'sc': 255, 'pr': 22},
+             'product': {'s': 'GRIB1', 'or': 80, 'ta': 2, 'pr': 1},
+             'level': {'s': 'GRIB1', 'lt': 1},
+             'timerange': {'s': 'GRIB1', 'ty': 0, 'un': 1, 'p1': 1, 'p2': 0},
+             'area': {'s': 'GRIB', 'va': {
+                 'Ni': 1083, 'Nj': 559, 'latfirst': -13050000, 'latlast': 12060000, 'latp': -47000000,
+                 'lonfirst': -25290000, 'lonlast': 23400000, 'lonp': 10000000, 'rot': 0, 'type': 10}},
+             'proddef': {'s': 'GRIB', 'va': {'tod': 1}},
+             'run': {'s': 'MINUTE', 'va': 0},
+             'summarystats': {'b': [2019, 6, 21, 0, 0, 0], 'e': [2019, 6, 21, 0, 0, 0], 'c': 1, 's': 1210914}},
+            {'origin': {'s': 'GRIB1', 'ce': 80, 'sc': 255, 'pr': 22},
+             'product': {'s': 'GRIB1', 'or': 80, 'ta': 2, 'pr': 1},
+             'level': {'s': 'GRIB1', 'lt': 1},
+             'timerange': {'s': 'GRIB1', 'ty': 0, 'un': 1, 'p1': 0, 'p2': 0},
+             'area': {'s': 'GRIB', 'va': {
+                 'Ni': 1083, 'Nj': 559, 'latfirst': -13050000, 'latlast': 12060000, 'latp': -47000000,
+                 'lonfirst': -25290000, 'lonlast': 23400000, 'lonp': 10000000, 'rot': 0, 'type': 10}},
+             'proddef': {'s': 'GRIB', 'va': {'tod': 1}},
+             'run': {'s': 'MINUTE', 'va': 0},
+             'summarystats': {'b': [2019, 6, 24, 0, 0, 0], 'e': [2019, 6, 24, 0, 0, 0], 'c': 1, 's': 1210914}},
+        ])
+
+        p = summary.to_python()
+        self.assertIn("items", p)
+
+        with arki.dataset.Session() as session:
+            cfg = arki.dataset.read_config("/home/enrico/lavori/arpa/arkimet/t/arkimet/lm5")
+            with session.dataset_reader(cfg=cfg) as reader:
+                summary = reader.query_summary('level:GRIB1,1;product:GRIB1,80,2,1')
+
+        p = summary.to_python()
+        self.assertIn("items", p)
+        self.assertEqual(len(p['items']), 0)
+
+        ds = arki.dataset.Reader({
+            "format": "arkimet",
+            "name": "issue230",
+            "path": "inbound/issue230.arkimet",
+            "type": "file",
+        })
+        summary = ds.query_summary()
+        self.assertIn("items", summary.to_python())
