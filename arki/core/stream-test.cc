6f7edbd75c5ae4fe6451d8c84143d0cdc02c9053
Enrico Zini <enrico@enricozini.org>
2021-03-23 10:22:00 +0100

    Added a draft timeout StreamOutput implementation and tests for it. Timing out is not yet implemented. refs: #252

--- arki/core/stream-test.cc
+++ arki/core/stream-test.cc
@@ -11,6 +11,48 @@ using namespace arki::tests;
 
 namespace {
 
+class BlockingSink
+{
+    int fds[2];
+    int pipe_sz;
+    std::unique_ptr<sys::NamedFileDescriptor> outfd;
+
+public:
+    BlockingSink()
+    {
+        // Create a pipe with a known buffer
+        // fds[1] will be the writing end
+        if (pipe(fds) < 0)
+            throw std::system_error(errno, std::system_category(), "cannot create a pipe");
+
+        // Shrink the output buffer as much as possible, and get its shrunk size
+        pipe_sz = fcntl(fds[1], F_SETPIPE_SZ, 0);
+        if (pipe_sz < 0)
+            throw std::system_error(errno, std::system_category(), "cannot shrink pipe size");
+        // Writing more than pipe_sz will now block unless we read
+
+        outfd.reset(new sys::NamedFileDescriptor(fds[1], "blocking test pipe"));
+    }
+
+    ~BlockingSink()
+    {
+        close(fds[0]);
+        close(fds[1]);
+    }
+
+    sys::NamedFileDescriptor& fd() { return *outfd; }
+    size_t pipe_size() const { return pipe_sz; }
+
+    /// Flush the pipe buffer until it's completely empty
+    void empty_buffer()
+    {
+        char buf[4096];
+        while (outfd->read(buf, 4096))
+            ;
+    }
+};
+
+
 class Tests : public TestCase
 {
     using TestCase::TestCase;
@@ -30,19 +72,65 @@ add_method("concrete", [] {
     sys::Tempfile tf1;
     tf1.write_all_or_throw(std::string("testfile"));
     writer->send_file_segment(tf1, 1, 6);
+    tf1.lseek(0);
+    writer->send_from_pipe(tf1);
 
     tf.lseek(0);
 
     char buf[256];
     wassert(actual(tf.read(buf, 256)) == 22u);
     buf[22] = 0;
-    wassert(actual(buf) == "testline\ntestbufestfil");
-    // virtual size_t send_vm2_line(const std::vector<uint8_t>& line) = 0;
-    // virtual size_t send_file_segment(arki::core::NamedFileDescriptor& fd, off_t offset, size_t size) = 0;
-    // virtual size_t send_buffer(const std::vector<uint8_t>& buf) = 0;
+    wassert(actual(buf) == "testline\ntestbufestfiltestfile");
+});
 
-    // static std::unique_ptr<StreamOutput> create(NamedFileDescriptor& out);
-    // static std::unique_ptr<StreamOutput> create(AbstractOutputFile& out);
+add_method("concrete_timeout", [] {
+    sys::Tempfile tf;
+    auto writer = StreamOutput::create(tf, 1000);
+    writer->send_line("testline", 8);
+    writer->send_buffer("testbuf", 7);
+    sys::Tempfile tf1;
+    tf1.write_all_or_throw(std::string("testfile"));
+    writer->send_file_segment(tf1, 1, 6);
+    tf1.lseek(0);
+    writer->send_from_pipe(tf1);
+
+    tf.lseek(0);
+
+    char buf[256];
+    wassert(actual(tf.read(buf, 256)) == 22u);
+    buf[22] = 0;
+    wassert(actual(buf) == "testline\ntestbufestfiltestfile");
+});
+
+add_method("concrete_timeout1", [] {
+    BlockingSink sink;
+    std::vector<uint8_t> filler(sink.pipe_size());
+
+    auto writer = StreamOutput::create(sink.fd(), 1);
+
+    // This won't block
+    writer->send_buffer(filler.data(), filler.size());
+    // This times out
+    wassert_throws(std::runtime_error, writer->send_buffer(" ", 1));
+
+    sink.empty_buffer();
+    wassert_throws(std::runtime_error, writer->send_line(filler.data(), filler.size()));
+
+    sink.empty_buffer();
+    writer->send_buffer(filler.data(), filler.size());
+    {
+        sys::Tempfile tf1;
+        tf1.write_all_or_throw(std::string("testfile"));
+        wassert_throws(std::runtime_error, writer->send_file_segment(tf1, 1, 6));
+    }
+
+    sink.empty_buffer();
+    writer->send_buffer(filler.data(), filler.size());
+    {
+        sys::Tempfile tf1;
+        tf1.write_all_or_throw(std::string("testfile"));
+        wassert_throws(std::runtime_error, writer->send_from_pipe(tf1));
+    }
 });
 
 add_method("abstract", [] {
@@ -54,9 +142,11 @@ add_method("abstract", [] {
     sys::Tempfile tf1;
     tf1.write_all_or_throw(std::string("testfile"));
     writer->send_file_segment(tf1, 1, 6);
+    tf1.lseek(0);
+    writer->send_from_pipe(tf1);
 
     std::string written(buffer.begin(), buffer.end());
-    wassert(actual(written) == "testline\ntestbufestfil");
+    wassert(actual(written) == "testline\ntestbufestfiltestfile");
 });
 
 }
--- arki/core/stream.cc
+++ arki/core/stream.cc
@@ -101,6 +101,101 @@ public:
 };
 
 
+class ConcreteTimeoutStreamOutput: public StreamOutput
+{
+    sys::NamedFileDescriptor& out;
+
+public:
+    ConcreteTimeoutStreamOutput(sys::NamedFileDescriptor& out, unsigned timeout_ms)
+        : out(out)
+    {
+        // TODO: set nonblocking
+    }
+    ~ConcreteTimeoutStreamOutput()
+    {
+        // TODO: if still open, reset as it was before
+    }
+
+    size_t send_line(const void* data, size_t size) override
+    {
+        // TODO: select loop with timeout
+        struct iovec todo[2] = {
+            { (void*)data, size },
+            { (void*)"\n", 1 },
+        };
+        ssize_t res = ::writev(out, todo, 2);
+        if (res < 0 || (unsigned)res != size + 1)
+            throw std::system_error(errno, std::system_category(), "cannot write " + std::to_string(size + 1) + " bytes to " + out.name());
+        return size + 1;
+    }
+
+    size_t send_file_segment(arki::core::NamedFileDescriptor& fd, off_t offset, size_t size) override
+    {
+        // TODO: select loop with timeout
+        fd.sendfile(out, offset, size);
+        acct::plain_data_read_count.incr();
+        // iotrace::trace_file(dirfd, offset, size, "streamed data");
+        return size;
+    }
+
+    size_t send_buffer(const void* data, size_t size) override
+    {
+        // TODO: select loop with timeout
+        // TODO: retry instead of throw
+        out.write_all_or_throw(data, size);
+        return size;
+
+#if 0
+        Sigignore ignpipe(SIGPIPE);
+        size_t pos = 0;
+        while (m_nextfd && pos < (size_t)res)
+        {
+            ssize_t wres = write(*m_nextfd, buf+pos, res-pos);
+            if (wres < 0)
+            {
+                if (errno == EPIPE)
+                {
+                    m_nextfd = nullptr;
+                } else
+                    throw_system_error("writing to destination file descriptor");
+            }
+            pos += wres;
+        }
+#endif
+    }
+
+    size_t send_from_pipe(int fd) override
+    {
+        // TODO: select loop with timeout
+#if defined(__linux__)
+#ifdef HAVE_SPLICE
+        // Try splice
+        ssize_t res = splice(subproc.get_stdout(), NULL, *m_nextfd, NULL, 4096*2, SPLICE_F_MORE);
+        if (res >= 0)
+            return res;
+
+        if (errno != EINVAL)
+            throw std::system_error(errno, std::system_category(), "cannot splice data to stream from a pipe");
+
+        // Splice is not supported: pass it on to the traditional method
+#endif
+#endif
+        // Fall back to read/write
+
+        // Read data from child
+        char buf[4096 * 8];
+        ssize_t res = read(fd, buf, 4096 * 8);
+        if (res < 0)
+            throw std::system_error(errno, std::system_category(), "cannot read data to stream from a pipe");
+        if (res == 0)
+            return 0;
+
+        // Pass it on
+        return send_buffer(buf, res);
+    }
+};
+
+
 class AbstractStreamOutput: public StreamOutput
 {
     AbstractOutputFile& out;
@@ -159,6 +254,11 @@ public:
 std::unique_ptr<StreamOutput> StreamOutput::create(NamedFileDescriptor& out)
 {
     return std::unique_ptr<StreamOutput>(new ConcreteStreamOutput(out));
+}
+
+std::unique_ptr<StreamOutput> StreamOutput::create(NamedFileDescriptor& out, unsigned timeout_ms)
+{
+    return std::unique_ptr<StreamOutput>(new ConcreteTimeoutStreamOutput(out, timeout_ms));
 }
 
 std::unique_ptr<StreamOutput> StreamOutput::create(AbstractOutputFile& out)
