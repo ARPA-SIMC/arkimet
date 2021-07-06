54636f36d7edc7d0f56baa8675a5a937ed14cbd7
Enrico Zini <enrico@enricozini.org>
2021-07-06 17:38:42 +0200

    Added descriptions for consts

--- arki/stream/fwd.h
+++ arki/stream/fwd.h
@@ -13,8 +13,20 @@ class TimedOut;
 
 struct SendResult
 {
+    /**
+     * The source file descriptor has been closed from the other end
+     */
     static constexpr uint32_t SEND_PIPE_EOF_SOURCE      = 0x01;
+
+    /**
+     * The destination file descriptor has been closed from the other end
+     */
     static constexpr uint32_t SEND_PIPE_EOF_DEST        = 0x02;
+
+    /**
+     * Sending from pipe stopped because the source file descriptor is set to
+     * nonblocking mode, and there is currently no data available to read
+     */
     static constexpr uint32_t SEND_PIPE_EAGAIN_SOURCE   = 0x04;
 
     size_t sent = 0;
