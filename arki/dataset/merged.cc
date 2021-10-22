19307d0642425c0b9de993cf8b0f67c49073b71f
Enrico Zini <enrico@enricozini.org>
2021-10-22 11:11:23 +0200

    Refactored merged dataset reading. Refs: #276

--- arki/dataset/merged.cc
+++ arki/dataset/merged.cc
@@ -68,7 +68,12 @@ public:
         return res;
     }
 
-    // Get must be done by the same thread that does pop
+    /**
+     * Peek at the first element in the queue. If the queue is empty, wait for
+     * an element to appear.
+     *
+     * Get must be done by the same thread that does pop
+     */
     const Metadata* get()
     {
         std::unique_lock<std::mutex> lock(mutex);
@@ -93,6 +98,10 @@ public:
     }
 };
 
+/**
+ * Thread that runs a dataset::Reader for one of the components of the merged
+ * dataset
+ */
 class ReaderThread
 {
 protected:
@@ -125,6 +134,9 @@ public:
     }
 };
 
+/**
+ * Reader thread for running a query_data
+ */
 class MetadataReader : public ReaderThread
 {
 public:
@@ -158,6 +170,9 @@ public:
     }
 };
 
+/**
+ * Reader thread for running a query_summary
+ */
 class SummaryReader : public ReaderThread
 {
 public:
@@ -186,6 +201,96 @@ public:
 };
 
 
+/**
+ * Pool of dataset MetadataReader threads
+ */
+class MetadataReaderPool
+{
+    // Reader threads that we manage
+    std::vector<MetadataReader> readers;
+
+    /**
+     * Sorted used for merging produced data
+     * 
+     * Note: we can assume that every dataset will give us data already sorted
+     * as required by the query, so here we just merge sorted data
+     */
+    std::shared_ptr<metadata::sort::Compare> sorter;
+
+public:
+    MetadataReaderPool(std::vector<std::shared_ptr<dataset::Reader>>& datasets, const dataset::DataQuery& query)
+        : readers(datasets.size()), sorter(query.sorter)
+    {
+        dataset::DataQuery subquery = query;
+        // Disable progress reporting on the query that we give to each component
+        // dataset, to avoid reporting data production twice
+        subquery.progress = nullptr;
+
+        if (!sorter)
+            sorter = metadata::sort::Compare::parse("");
+
+        for (size_t i = 0; i < datasets.size(); ++i)
+            readers[i].start(datasets[i], subquery);
+    }
+
+    /**
+     * Get the next element from reader threads, in sorter order
+     */
+    std::shared_ptr<Metadata> pop()
+    {
+        // Look in all dataset queues for the metadata with the smallest value
+        const Metadata* minmd = nullptr;
+        MetadataReader* selected = nullptr;
+        for (auto& reader: readers)
+        {
+            const Metadata* md = reader.mdbuf.get();
+            if (!md) continue;
+            if (!minmd || sorter->compare(*md, *minmd) < 0)
+            {
+                minmd = md;
+                selected = &reader;
+            }
+        }
+
+        // When there's nothing more to read, we exit
+        if (minmd == nullptr)
+        {
+            shutdown();
+            return std::shared_ptr<Metadata>();
+        }
+
+        // Pop the selected value out of its queue
+        return selected->mdbuf.pop();
+    }
+
+    void cancel()
+    {
+        for (auto& r: readers)
+            r.stop_producing();
+        while (pop())
+            ;
+    }
+
+    void shutdown()
+    {
+        std::vector<std::string> errors;
+        for (auto& reader: readers)
+        {
+            try {
+                reader.join();
+            } catch (std::exception& e) {
+                errors.push_back(e.what());
+                continue;
+            }
+            if (!reader.errorbuf.empty())
+                errors.push_back(reader.errorbuf);
+        }
+        if (!errors.empty())
+            throw std::runtime_error("problems running metadata queries on multiple datasets: " + str::join("; ", errors.begin(), errors.end()));
+    }
+};
+
+
 Dataset::Dataset(std::shared_ptr<dataset::Pool> pool)
     : dataset::Dataset(pool->session(), "merged")
 {
@@ -207,88 +312,43 @@ Reader::~Reader()
 
 std::string Reader::type() const { return "merged"; }
 
-bool Reader::impl_query_data(const dataset::DataQuery& q, metadata_dest_func dest)
+bool Reader::impl_query_data(const dataset::DataQuery& query, metadata_dest_func dest)
 {
-    dataset::TrackProgress track(q.progress);
+    dataset::TrackProgress track(query.progress);
     dest = track.wrap(dest);
 
     auto& datasets = dataset().datasets;
 
     // Handle the trivial case of only one dataset
     if (datasets.size() == 1)
-        return track.done(datasets[0]->query_data(q, dest));
-
-    dataset::DataQuery subquery = q;
-    subquery.progress = nullptr;
-
-    bool canceled = false;
-    // TODO: we need to join or detach the threads, otehrwise if we raise an exception before the join further down, we get
-    // https://stackoverflow.com/questions/7381757/c-terminate-called-without-an-active-exception
+        return track.done(datasets[0]->query_data(query, dest));
 
     // Start all the readers
-    std::vector<MetadataReader> readers(datasets.size());
-    for (size_t i = 0; i < datasets.size(); ++i)
-        readers[i].start(datasets[i], subquery);
+    MetadataReaderPool readers(datasets, query);
 
-    // Output items in time-sorted order or in the order asked by subquery
-    // Note: we assume that every dataset will give us data sorted as subquery
-    // asks, so here we just merge sorted data
-    std::shared_ptr<metadata::sort::Compare> sorter = subquery.sorter;
-    if (!sorter)
-        sorter = metadata::sort::Compare::parse("");
+    // TODO: we need to join or detach the threads, otherwise if we raise an exception before the join further down, we get
+    // https://stackoverflow.com/questions/7381757/c-terminate-called-without-an-active-exception
 
-    std::vector<std::string> errors;
+    bool canceled = false;
     while (true)
     {
-        // Look in all dataset queues for the metadata with the smallest value
-        const Metadata* minmd = nullptr;
-        int minmd_idx = 0;
-        for (size_t i = 0; i < datasets.size(); ++i)
-        {
-            // TODO: what if a dataset has not started producing yet?
-            const Metadata* md = readers[i].mdbuf.get();
-            if (!md) continue;
-            if (!minmd || sorter->compare(*md, *minmd) < 0)
-            {
-                minmd = md;
-                minmd_idx = i;
-            }
-        }
-        // When there's nothing more to read, we exit
-        if (minmd == nullptr) break;
-        auto md = readers[minmd_idx].mdbuf.pop();
-        if (!canceled)
-        {
-            bool ok;
-            try {
-                ok = dest(md);
-            } catch (std::exception& e) {
-                ok = false;
-                errors.push_back(e.what());
-            }
-            if (!ok)
-            {
-                canceled = true;
-                for (auto& r: readers)
-                    r.stop_producing();
-            }
-        }
-    }
+        auto md = readers.pop();
+        if (!md) break;
 
-    // Collect all the results
-    for (auto& reader: readers)
-    {
+        bool ok;
         try {
-            reader.join();
+            ok = dest(md);
         } catch (std::exception& e) {
-            errors.push_back(e.what());
-            continue;
+            readers.cancel();
+            throw;
         }
-        if (!reader.errorbuf.empty())
-            errors.push_back(reader.errorbuf);
+
+        if (!ok)
+        {
+            canceled = true;
+            readers.cancel();
+        }
     }
-    if (!errors.empty())
-        throw_consistency_error("running metadata queries on multiple datasets", str::join("; ", errors.begin(), errors.end()));
 
     return track.done(canceled);
 }
@@ -310,7 +370,7 @@ void Reader::impl_query_summary(const Matcher& matcher, Summary& summary)
         readers[i].start(datasets[i], matcher);
 
     // Collect all the results
-    vector<string> errors;
+    std::vector<std::string> errors;
     for (auto& reader: readers)
     {
         reader.join();
