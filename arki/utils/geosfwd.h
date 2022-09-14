9c93218ca2e5eec7fd2eaff67c29ccbf8aca2860
Enrico Zini <enrico@enricozini.org>
2022-09-14 11:40:18 +0200

    Cleanup geosfwd header. Refs: #291

--- arki/utils/geosfwd.h
+++ arki/utils/geosfwd.h
@@ -1,27 +1,7 @@
 #ifndef ARKI_UTILS_GEOSFWD_H
 #define ARKI_UTILS_GEOSFWD_H
 
-/*
- * utils-geosfwd - libgeos forward definitions
- *
- * Copyright (C) 2009--2011  ARPAE-SIMC <simc-urp@arpae.it>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Author: Enrico Zini <enrico@enricozini.com>
- */
+/// libgeos forward definitions
 
 #include <arki/libconfig.h>
 
@@ -29,42 +9,14 @@
  * The C++ interface of GEOS is not guaranteed to be stable.
  *
  * The C interface of GEOS is guaranteed to be stable, but it requires
- * reimplementing extensive resource management, which would mean re-wrapping
- * the C GEOS API in C++ wrappers that do proper RAII resource management.
+ * re-wrapping in C++ to get proper RAII resource management.
  *
- * At the moment it seems easier to use a local thin adapter layer to make the
- * C++ GEOS API changes manageable. This may change in the future depending on
- * how extensive future GEOS API breaks are going to be.
+ * We used a thin layer of compatibility glue to deal with the C++ API.
+ * However, due to packaging issues with the C++ APIs (see
+ * https://github.com/ARPA-SIMC/arkimet/issues/291), we now have a C++ wrapper
+ * to the C wrapper to the C++ GEOS API.
  */
 
-// Forward-declare geos components
-#ifdef HAVE_GEOS
-#include <geos/version.h>
-
-namespace geos {
-#if GEOS_VERSION_MAJOR < 3
-struct Geometry;
-struct GeometryFactory;
-struct Coordinate;
-struct CoordinateArraySequence;
-struct LinearRing;
-struct WKTReader;
-#else
-namespace geom {
-struct Geometry;
-struct GeometryFactory;
-struct Coordinate;
-struct CoordinateArraySequence;
-struct LinearRing;
-}
-namespace io {
-struct WKTReader;
-}
-#endif
-}
-#endif
-
-// Bring geos components into a local, stable namespace
 namespace arki {
 namespace utils {
 namespace geos {
@@ -72,27 +24,7 @@ namespace geos {
 class GeometryVector;
 class Geometry;
 class WKTReader;
-
-#ifdef HAVE_GEOS
-
-#if GEOS_VERSION_MAJOR < 3
-using ::geos::GeometryFactory;
-using ::geos::Coordinate;
-using ::geos::CoordinateArraySequence;
-using ::geos::LinearRing;
-#else
-using ::geos::geom::GeometryFactory;
-using ::geos::geom::Coordinate;
-using ::geos::geom::CoordinateArraySequence;
-using ::geos::geom::LinearRing;
-#endif
-
-#else
-
-struct GeometryFactory {};
-struct Coordinate {};
-
-#endif
+class WKTWriter;
 
 }
 }
