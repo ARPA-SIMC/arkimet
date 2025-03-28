a8d706c060b7e5aac4a1fe8535b4bfd78e06c1f3
Enrico Zini <enrico@enricozini.org>
2025-03-28 17:29:06 +0100

    Use OverrideEnvironment from wobble

--- arki/core/tests.cc
+++ arki/core/tests.cc
@@ -17,40 +17,6 @@ using arki::core::Time;
 namespace arki {
 namespace tests {
 
-OverrideEnvironment::OverrideEnvironment(const std::string& name)
-    : name(name)
-{
-    char* old_val = getenv(name.c_str());
-    if (old_val)
-    {
-        was_set = true;
-        orig_value = old_val;
-    }
-    if (::unsetenv(name.c_str()) == -1)
-        throw_system_error("unsetenv " + name + " failed");
-}
-
-OverrideEnvironment::OverrideEnvironment(const std::string& name, const std::string& value)
-    : name(name)
-{
-    char* old_val = getenv(name.c_str());
-    if (old_val)
-    {
-        was_set = true;
-        orig_value = old_val;
-    }
-    if (::setenv(name.c_str(), value.c_str(), 1) == -1)
-        throw_system_error("setenv " + name + "=" + value + " failed");
-}
-
-OverrideEnvironment::~OverrideEnvironment()
-{
-    if (was_set)
-        setenv(name.c_str(), orig_value.c_str(), 1);
-    else
-        unsetenv(name.c_str());
-}
-
 void ActualTime::operator==(const std::string& expected) const
 {
     operator==(Time::create_iso8601(expected));
